93b126ffc346fdff0b311f99c25092bfa4c14183
Peter C Winter <agile-deployer@users.noreply.github.com>
2020-02-09 14:17:06 +0000

    Update InstallPHPBase.sh

--- installscripts/InstallPHPBase.sh
+++ installscripts/InstallPHPBase.sh
@@ -31,11 +31,22 @@ fi
 
 if ( [ "${BUILDOS}" = "ubuntu" ] )
 then
-    /usr/bin/add-apt-repository ppa:ondrej/php
+    /usr/bin/add-apt-repository -y ppa:ondrej/php
     /usr/bin/apt -qq -y update
     #removed php-mcrypt php7.2
     /usr/bin/apt-get -qq -y install php${phpversion}-fpm php${phpversion}-cli php${phpversion}-common php${phpversion}-dev  php${phpversion}-json php${phpversion}-opcache php${phpversion}-mysql php${phpversion}-phpdbg php${phpversion}-mbstring php${phpversion}-gd php${phpversion}-imap php${phpversion}-ldap php${phpversion}-pgsql php${phpversion}-pspell php${phpversion}-tidy php${phpversion}-intl php${phpversion}-gd php${phpversion}-curl php${phpversion}-zip php${phpversion}-xml php-mysql php${phpversion}-memcache php${phpversion}-redis
+    #If php did not install at the version we chose for example, if you chose to install php7.4 on ubuntu 19.04 it will not install
+    #This is  potential configuration oversight if someone doesn't realise what versions of php are supported by a particular OS
+    #So, in this case, check if php has been installed at our desired version and if it isn't do the best that we can
+    installedphpversion="`/usr/bin/php -v | /bin/grep "^PHP" | /usr/bin/awk '{print $2}' | /usr/bin/awk -F'.' '{print $1,$2}' | /bin/sed 's/ /\./g'`"
 
+    if ( [ "${installedphpversion}" != "${phpversion}" ] )
+    then
+        /usr/bin/apt-get -qq -y install php-fpm php-cli php-common php-dev  php-json php-opcache php-mysql php-phpdbg php-mbstring php-gd php-imap php-ldap php-pgsql php-pspell php-tidy php-intl php-gd php-curl php-zip php-xml php-mysql php-memcache php-redis
+        installedphpversion="`/usr/bin/php -v | /bin/grep "^PHP" | /usr/bin/awk '{print $2}' | /usr/bin/awk -F'.' '{print $1,$2}' | /bin/sed 's/ /\./g'`"
+        /bin/rm ${HOME}/.ssh/PHP_VERSION*
+        /bin/touch ${HOME}/.ssh/PHP_VERSION:${installedphpversion}
+    fi
 fi
 
 if ( [ "${BUILDOS}" = "debian" ] )
@@ -44,8 +55,24 @@ then
     /usr/bin/apt -qq -y install apt-transport-https lsb-release ca-certificates
     /usr/bin/wget -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg
     /bin/sh -c 'echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" > /etc/apt/sources.list.d/php.list'
+
     /usr/bin/apt -qq -y update
+
     /usr/bin/apt-get -qq -y install php${phpversion}-fpm php${phpversion}-cli php${phpversion}-common php${phpversion}-dev  php${phpversion}-json php${phpversion}-opcache php${phpversion}-mysql php${phpversion}-phpdbg php${phpversion}-mbstring php${phpversion}-gd php${phpversion}-imap php${phpversion}-ldap php${phpversion}-pgsql php${phpversion}-pspell php${phpversion}-tidy php${phpversion}-intl php${phpversion}-gd php${phpversion}-curl php${phpversion}-zip php${phpversion}-xml php-mysql php${phpversion}-memcache php${phpversion}-redis
+
+
+    #If php did not install at the version we chose for example, if you chose to install php7.4 on ubuntu 19.04 it will not install
+    #This is  potential configuration oversight if someone doesn't realise what versions of php are supported by a particular OS
+    #So, in this case, check if php has been installed at our desired version and if it isn't do the best that we can
+    installedphpversion="`/usr/bin/php -v | /bin/grep "^PHP" | /usr/bin/awk '{print $2}' | /usr/bin/awk -F'.' '{print $1,$2}' | /bin/sed 's/ /\./g'`"
+
+    if ( [ "${installedphpversion}" != "${phpversion}" ] )
+    then
+        /usr/bin/apt-get -qq -y install php-fpm php-cli php-common php-dev  php-json php-opcache php-mysql php-phpdbg php-mbstring php-gd php-imap php-ldap php-pgsql php-pspell php-tidy php-intl php-gd php-curl php-zip php-xml php-mysql php-memcache php-redis
+        installedphpversion="`/usr/bin/php -v | /bin/grep "^PHP" | /usr/bin/awk '{print $2}' | /usr/bin/awk -F'.' '{print $1,$2}' | /bin/sed 's/ /\./g'`"
+        /bin/rm ${HOME}/.ssh/PHP_VERSION*
+        /bin/touch ${HOME}/.ssh/PHP_VERSION:${installedphpversion}
+    fi
 fi
 
 
