ec7b5c47e7cebdd6ee77df86ff89107429dd2661
Peter C Winter <agile-deployer@users.noreply.github.com>
2020-02-20 21:49:56 +0000

    Create InstallCloudhostTools.sh

---
+++ providerscripts/cloudhost/InstallCloudhostTools.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+##############################################################################
+# Description: This script will install the tools for the selected cloudhost.
+# It is with these tools that servers can be manipulated.
+# Author: Peter Winter
+# Date: 12/01/2017
+##############################################################################
+# License Agreement:
+# This file is part of The Agile Deployment Toolkit.
+# The Agile Deployment Toolkit is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# The Agile Deployment Toolkit is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with The Agile Deployment Toolkit.  If not, see <http://www.gnu.org/licenses/>.
+#############################################################################
+#############################################################################
+
+#Configure the machine for the current provider. Each new provider that is added will need a config process like these to be added
+#here
+
+BUILDOS="`/bin/ls ${HOME}/.ssh/BUILDOS:* | /usr/bin/awk -F':' '{print $NF}'`"
+
+if ( [ "${CLOUDHOST}" = "exoscale" ] )
+then
+    /usr/bin/touch ${HOME}/EXOSCALE
+fi
+if ( [ "${CLOUDHOST}" = "digitalocean" ] )
+then
+    /usr/bin/touch ${HOME}/DROPLET
+fi
+
+if ( [ "${CLOUDHOST}" = "linode" ] )
+then
+    /usr/bin/touch ${HOME}/LINODE
+fi
+
+if ( [ "${CLOUDHOST}" = "vultr" ] )
+then
+    /usr/bin/touch ${HOME}/VULTR
+fi
+if ( [ "${CLOUDHOST}" = "aws" ] )
+then
+    ${HOME}/installscripts/InstallJQ.sh ${BUILDOS}
+    ${HOME}/installscripts/InstallAWSCLI.sh ${BUILDOS}
+    /usr/bin/touch ${HOME}/AWS
+fi 
