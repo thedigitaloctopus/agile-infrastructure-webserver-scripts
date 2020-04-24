eff25632b14db02836f723f4500b588711c774cb
Peter C Winter <agile-deployer@users.noreply.github.com>
2020-04-24 18:37:57 +0100

    Update InstallPythonPIP.sh

--- installscripts/InstallPythonPIP.sh
+++ installscripts/InstallPythonPIP.sh
@@ -28,10 +28,12 @@ fi
 if ( [ "${BUILDOS}" = "ubuntu" ] )
 then
     /usr/bin/apt-get -qq -y -f install python3-pip
+    /usr/bin/ln /usr/bin/python3 /usr/bin/python
 fi
 
 if ( [ "${BUILDOS}" = "debian" ] )
 then
     /usr/bin/apt-get -qq -y -f install python3-pip
+    /usr/bin/ln /usr/bin/python3 /usr/bin/python
 fi
 
