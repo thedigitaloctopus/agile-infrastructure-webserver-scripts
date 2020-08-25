14a8cfb85326e2413394cdc4da7e6b6200bdd78a
Peter C Winter <agile-deployer@users.noreply.github.com>
2020-08-25 22:02:03 +0100

    Create BackupEFSToDatastoreFromCron.sh

---
+++ cron/BackupEFSToDatastoreFromCron.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+############################################################################################################################
+# Description: This script will make a backup of the assets directory within S3
+# Date: 16/11/2016
+# Author: Peter Winter
+###########################################################################################################
+# License Agreement:
+# This file is part of The Agile Deployment Toolkit.
+# The Agile Deployment Toolkit is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# The Agile Deployment Toolkit is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with The Agile Deployment Toolkit.  If not, see <http://www.gnu.org/licenses/>.
+#######################################################################################################
+#######################################################################################################
+#set -x
+
+periodicity="${1}"
+buildidentifier="${2}"
+
+lockfile=${HOME}/config/backupefstodatastorelock.file
+
+/bin/rm ${lockfile}
+
+/bin/sleep 300 
+
+if ( [ ! -f ${lockfile} ] )
+then
+    /usr/bin/touch ${lockfile}
+    ${HOME}/providerscripts/datastore/BackupEFSToDatastore.sh
+    /bin/rm ${lockfile}
+else
+    /bin/echo "script already running"
+fi
