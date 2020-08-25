18011cc1724ac1e7d04f2f6adf7980ff4a04bd8e
Peter C Winter <agile-deployer@users.noreply.github.com>
2020-08-25 22:04:50 +0100

    Update BackupS3ToDatastoreFromCron.sh

--- cron/BackupS3ToDatastoreFromCron.sh
+++ cron/BackupS3ToDatastoreFromCron.sh
@@ -25,6 +25,12 @@ buildidentifier="${2}"
 
 lockfile=${HOME}/config/backups3todatastorelock.file
 
+/bin/rm ${lockfile}
+
+delay=(10 20 30 40 50 60 70 80 90 100 110 120 130 140 150 160 170 180 190 200 210 220 230 240 250 260 270 280 290 300)
+delay="`/bin/echo ${delay[RANDOM%30]}`"
+sleep ${delay}
+
 if ( [ ! -f ${lockfile} ] )
 then
     /usr/bin/touch ${lockfile}
