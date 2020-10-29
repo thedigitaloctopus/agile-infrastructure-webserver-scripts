b5c4a1f351478fdd91ef1696afe4b99086dbdfbc
Peter C Winter <agile-deployer@users.noreply.github.com>
2020-10-29 13:19:21 +0000

    Update ConfigureDBAccess.sh

--- providerscripts/application/configuration/wordpress/ConfigureDBAccess.sh
+++ providerscripts/application/configuration/wordpress/ConfigureDBAccess.sh
@@ -49,6 +49,10 @@ then
     /bin/cp ${HOME}/providerscripts/application/configuration/wordpress-htaccess.txt /var/www/html/.htaccess
     /bin/chown www-data.www-data /var/www/html/.htaccess
     /bin/chmod 440 /var/www/html/.htaccess
+    if ( [ -f /var/www/html/wordfence-waf.php ] )
+    then
+        /bin/cat ${HOME}/providerscripts/application/configuration/wordfence.txt >> /var/www/html/.htaccess
+    fi
 fi
 
 if ( [ ! -f /var/www/html/.user.ini ] && [ -f /var/www/html/wordfence-waf.php ] )
