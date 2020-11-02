dde758e578f809d3f416fc9b10215724baeaf631
Peter C Winter <agile-deployer@users.noreply.github.com>
2020-11-02 02:15:49 +0000

    Update SetupAssetsStore.sh

--- providerscripts/datastore/SetupAssetsStore.sh
+++ providerscripts/datastore/SetupAssetsStore.sh
@@ -27,6 +27,7 @@ if ( [ -f ${HOME}/.ssh/PERSISTASSETSTOCLOUD:0 ] && [ ! -f ${HOME}/runtime/S3BUCK
 then
     domainspecifier="`/bin/echo ${WEBSITE_URL} | /usr/bin/awk -F'.' '{ for(i = 1; i <= NF; i++) { print $i; } }' | /usr/bin/cut -c1-3 | /usr/bin/tr '\n' '-' | /bin/sed 's/-//g'`"
     /usr/bin/s3cmd mb s3://${domainspecifier}
+    /bin/touch ${HOME}/.ssh/ASSETSBUCKET:${domainspecifier}
     /bin/touch ${HOME}/runtime/S3BUCKETSET
     exit
 else
