082b276faf6d9a94534387ad3c14d903e2e03463
Agile Deployer <agile-deployer@users.noreply.github.com>
2021-05-28 19:27:33 +0100

    Update InstallPHPBase.sh

--- installscripts/InstallPHPBase.sh
+++ installscripts/InstallPHPBase.sh
@@ -48,8 +48,12 @@ then
 
         if ( [ "${installedphpversion}" != "${phpversion}" ] )
         then
-            /usr/bin/apt-get -qq -y install php-fpm php-cli php-common php-dev php-json php-opcache php-mysqli php-phpdbg php-mbstring php-gd php-imap php-ldap php-pgsql php-pspell php-tidy php-intl php-gd php-curl php-zip php-xml
+            /usr/bin/apt-get -qq -y purge php${installedphpversion}
+            /usr/bin/apt-get -qq -y autoclean
+            /usr/bin/apt-get -qq -y autoremove
+            /bin/rm -r /etc/php/${installedphpversion}
         fi
+        /usr/bin/apt-get -qq -y install php-fpm php-cli php-common php-dev php-json php-opcache php-mysqli php-phpdbg php-mbstring php-gd php-imap php-ldap php-pgsql php-pspell php-tidy php-intl php-gd php-curl php-zip php-xml
     elif ( [ "${BUILDOSVERSION}" = "20.04" ] )
     then
         /usr/bin/add-apt-repository -y ppa:ondrej/php
