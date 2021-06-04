bdb592961f50ac14d63cfe35b2e7b8e8a4bffaca
Agile Deployer <agile-deployer@users.noreply.github.com>
2021-06-04 08:36:29 +0100

    Update TrustCloudflareProxyApache.sh

--- providerscripts/dns/TrustCloudflareProxyApache.sh
+++ providerscripts/dns/TrustCloudflareProxyApache.sh
@@ -12,13 +12,13 @@ then
 
     /bin/echo "# - IPv4" >> $CLOUDFLARE_FILE_PATH;
     for i in `curl https://www.cloudflare.com/ips-v4`; do
-        /bin/echo "RemoteIPTrustProxy $i" >> $CLOUDFLARE_FILE_PATH;
+        /bin/echo "RemoteIPTrustedProxy $i" >> $CLOUDFLARE_FILE_PATH;
     done
 
     /bin/echo "" >> $CLOUDFLARE_FILE_PATH;
     /bin/echo "# - IPv6" >> $CLOUDFLARE_FILE_PATH;
     for i in `curl https://www.cloudflare.com/ips-v6`; do
-        /bin/echo "RemoteIPTrustProxy $i" >> $CLOUDFLARE_FILE_PATH;
+        /bin/echo "RemoteIPTrustedProxy $i" >> $CLOUDFLARE_FILE_PATH;
     done
     
 fi
