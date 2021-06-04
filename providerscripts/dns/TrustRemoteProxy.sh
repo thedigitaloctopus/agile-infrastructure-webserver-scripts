4f597a762b4b44e18f668771b79ad49f3427cdfb
Agile Deployer <agile-deployer@users.noreply.github.com>
2021-06-04 12:25:14 +0100

    Update TrustRemoteProxy.sh

--- providerscripts/dns/TrustRemoteProxy.sh
+++ providerscripts/dns/TrustRemoteProxy.sh
@@ -1,4 +1,32 @@
 #!/bin/bash
+######################################################################################
+# Description: When we use a proxy service the service might rotate IP addresses that it
+# connects to our orgin server from (apache or nginx) and some application have checks 
+# to make sure that the requester ip hasn't changed. In such a case our application will
+# fail (Joomla 4) is one example that has strict ip checking. So, we need to set the origin
+# IP to be the real ip of our machine irrespective of which proxy ip address the request
+# has been routed through. 
+# You can find out more about this (because it stumped me for a couple of days) here:
+# https://github.com/ergin/nginx-cloudflare-real-ip
+# https://support.cloudflare.com/hc/en-us/articles/200170786-Restoring-original-visitor-IPs
+# Date: 07/06/2021
+# Author: Peter Winter
+#######################################################################################
+# License Agreement:
+# This file is part of The Agile Deployment Toolkit.
+# The Agile Deployment Toolkit is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# The Agile Deployment Toolkit is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with The Agile Deployment Toolkit.  If not, see <http://www.gnu.org/licenses/>.
+####################################################################################
+####################################################################################
+#set -x
 
 if ( [ -f ${HOME}/.ssh/DNSCHOICE:cloudflare ] )
 then
