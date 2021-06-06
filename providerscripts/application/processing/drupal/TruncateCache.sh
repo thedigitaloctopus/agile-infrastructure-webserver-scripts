173f082879588e99e3db077aff79d461e3df9b79
Agile Deployer <agile-deployer@users.noreply.github.com>
2021-06-06 23:15:55 +0100

    Update TruncateCache.sh

--- providerscripts/application/processing/drupal/TruncateCache.sh
+++ providerscripts/application/processing/drupal/TruncateCache.sh
@@ -4,7 +4,7 @@ if ( [ -f ${HOME}/.ssh/DATABASEINSTALLATIONTYPE:Maria ] || [ -f ${HOME}/.ssh/DAT
 then
     prefix="`/bin/cat /var/www/html/dpb.dat`"
 
-    ${HOME}/providerscripts/utilities/ConnectToRemoteMYSQLDB.sh "TRUNCATE ${prefix}_cache_bootstrap;TRUNCATE ${prefix}_cache_config; TRUNCATE ${prefix}_cache_container;TRUNCATE ${prefix}_cache_data;TRUNCATE ${prefix}_cache_default;TRUNCATE ${prefix}_cache_discovery;TRUNCATE ${prefix}_cache_dynamic_page_cache;TRUNCATE ${prefix}_cache_entity;TRUNCATE ${prefix}_cache_menu; TRUNCATE ${prefix}_cache_page;TRUNCATE ${prefix}_cache_render;TRUNCATE ${prefix}_cache_toolbar;TRUNCATE ${prefix}_cachetags;"
+    ${HOME}/providerscripts/utilities/ConnectToRemoteMYSQLDB.sh "TRUNCATE ${prefix}_cache_bootstrap;TRUNCATE ${prefix}_cache_config; TRUNCATE ${prefix}_cache_container;TRUNCATE ${prefix}_cache_data;TRUNCATE ${prefix}_cache_default;TRUNCATE ${prefix}_cache_discovery;TRUNCATE ${prefix}_cache_dynamic_page_cache;TRUNCATE ${prefix}_cache_entity;TRUNCATE ${prefix}_cache_menu; TRUNCATE ${prefix}_cache_page;TRUNCATE ${prefix}_cache_render;TRUNCATE ${prefix}_cachetags;"
 
     if ( [ "$?" = "0" ] )
     then
