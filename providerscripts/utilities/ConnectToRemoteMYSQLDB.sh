bc93e71c675e078a4709fdc13132849196d8ac33
Agile Deployer <agile-deployer@users.noreply.github.com>
2021-06-08 16:14:16 +0100

    Update ConnectToRemoteMYSQLDB.sh

--- providerscripts/utilities/ConnectToRemoteMYSQLDB.sh
+++ providerscripts/utilities/ConnectToRemoteMYSQLDB.sh
@@ -1,4 +1,27 @@
 #!/bin/sh
+######################################################################################
+# Author: Peter Winter
+# Date :  07/06/2021
+# Description: Connect to the remote MySQL or MariaDB database and execute a query if 
+# present. It uses the provided credentials that are part of the build process.
+# It takes two parameters, the first is any command to execute and the second is whether
+# it is to be of raw format or not. 
+#####################################################################################
+# License Agreement:
+# This file is part of The Agile Deployment Toolkit.
+# The Agile Deployment Toolkit is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# The Agile Deployment Toolkit is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with The Agile Deployment Toolkit.  If not, see <http://www.gnu.org/licenses/>.
+#######################################################################################
+#######################################################################################
+#set -x
 
 SERVER_USER="`/bin/ls ${HOME}/.ssh/SERVERUSER:* | /usr/bin/awk -F':' '{print $NF}'`"
 SERVER_USER_PASSWORD="`/bin/ls ${HOME}/.ssh/SERVERUSERPASSWORD:* | /usr/bin/awk -F':' '{print $NF}'`"
