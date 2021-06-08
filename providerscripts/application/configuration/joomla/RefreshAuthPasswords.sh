1abcaf6a580cbb2dc8a5adfe40d99b6e051b8403
Agile Deployer <agile-deployer@users.noreply.github.com>
2021-06-08 15:16:31 +0100

    Update RefreshAuthPasswords.sh

--- providerscripts/application/configuration/joomla/RefreshAuthPasswords.sh
+++ providerscripts/application/configuration/joomla/RefreshAuthPasswords.sh
@@ -1,56 +1,26 @@
-#set -x
+
 
 if ( [ ! -f /etc/nginx/.htpasswd ] )
 then
     /bin/touch /etc/nginx/.htpasswd
 fi
 
+#Update ConnectToRemoteDBMYSQLDB.sh with the "raw" output
+
 user_table_name="`/bin/cat /var/www/html/dpb.dat`_users"
-credentials="`${HOME}/providerscripts/utilities/ConnectToRemoteMYSQLDB.sh "select name,password from ${user_table_name}" raw`"
-credentials="${credentials} user1 password1"
-usernames="`/bin/echo ${credentials} | /bin/tr ' ' '\n' | /bin/sed 'n; d'`"
-passwords="`/bin/echo ${credentials} | /bin/tr ' ' '\n' | /bin/sed '1d; n; d'`"
+usernames="`${HOME}/providerscripts/utilities/ConnectToRemoteMYSQLDB.sh "select name from ${user_table_name}" raw`"
+passwords="`${HOME}/providerscripts/utilities/ConnectToRemoteMYSQLDB.sh "select password from ${user_table_name}" raw`"
 
-credentials="`/bin/echo '${credentials}' | /bin/sed 's/\./\\./g' | /bin/sed 's/\[/\\[/g' | /bin/sed 's/\*/\\*/g' | /bin/sed 's;/;\\/;g' | /bin/sed 's/\$/\\\$/g' | /bin/sed 's/(/\\(/g' | /bin/sed 's/)/\\)/g'`"
-
-echo $credentials
-
-exit
-
-
+count="0"
 for username in ${usernames}
 do
-   credentials="`/bin/echo ${credentials} | /bin/sed "s/${username} /${username}\:gap1\:/g"`"
-done
-
-for password in ${passwords}
-do
-   credentials="`/bin/echo ${credentials} | /bin/sed "s/${password} /${password}\|gap2|/g"`"
-done
-
-/bin/echo $credentials | /bin/sed 's/\:gap1\:/\:/g' | /bin/sed 's/|gap2|/\n/g' |  /bin/sed '/^$/d' > /etc/nginx/.htpasswd
-
-exit
-
-number_of_usernames="`/bin/echo ${usernames} | /usr/bin/wc -w`"
-
-count="1"
-
-while ( [ "${count}" -lt "${number_of_usernames}" ] )
-do
-    username="`/bin/echo "${usernames}" | cut -d " " -f ${count}`"
-    password="`/bin/echo "${passwords}" | cut -d " " -f ${count}`"
-
-    username="`/bin/echo "${usernames}" | awk 'print ${count}'`"
-    password="`/bin/echo "${passwords}" | awk 'print ${count}'`"
-
-    echo "$username $password"
-
-    exit
-
-    if ( [ "`/bin/cat /etc/nginx/.htpasswd | /bin/grep ${username}`" = "" ] &&  [ "`/bin/cat /etc/nginx/.htpasswd | /bin/grep ${password}`" = "" ] )
-    then
-        /bin/echo -n '${username}:${password}:' >> /etc/nginx/.htpasswd
-    fi
     count="`/usr/bin/expr ${count} + 1`"
+    password="`/bin/echo "${passwords}" | /usr/bin/cut -d " " -f ${count}`"
+    matchablepassword="`/bin/echo ${password} | /bin/sed 's/$/\$/g'`"
+
+    if ( [ "`/bin/cat /tmp/credentials | /bin/grep "${username}"`" = "" ] || [ "`/bin/cat /tmp/credentials | /bin/grep ${matchablepassword}`" = "" ] )
+    then
+        /bin/sed -i "/${username}/d" /etc/nginx/.htpasswd
+        /bin/echo "${username}:${password}" >> /etc/nginx/.htpasswd
+    fi
 done
