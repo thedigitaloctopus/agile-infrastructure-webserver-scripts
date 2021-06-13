4726a9060a7faca367df4ba34612fdf4bd4eaa40
Agile Deployer <agile-deployer@users.noreply.github.com>
2021-06-14 00:58:55 +0100

    Update CheckSessions.sh

--- providerscripts/application/processing/drupal/CheckSessions.sh
+++ providerscripts/application/processing/drupal/CheckSessions.sh
@@ -1,3 +1,25 @@
+#!/bin/sh
+######################################################################################
+# Author: Peter Winter
+# Date :  07/07/2016
+# Description: This is a simple way of checking that drupal has initialised a session correctly. 
+# The way I check is just to check if any sessions have been added to the _sessions table. 
+# If sessions exist, then, we can assume that sessions are being created correctly. 
+#####################################################################################
+# License Agreement:
+# This file is part of The Agile Deployment Toolkit.
+# The Agile Deployment Toolkit is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# The Agile Deployment Toolkit is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with The Agile Deployment Toolkit.  If not, see <http://www.gnu.org/licenses/>.
+#######################################################################################
+#######################################################################################
 set -x
 
 if ( [ -f ${HOME}/.ssh/DATABASEINSTALLATIONTYPE:Maria ] || [ -f ${HOME}/.ssh/DATABASEINSTALLATIONTYPE:MySQL ] )
