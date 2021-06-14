30b100d12a05d63f75d4d8d7c06e7ef2138163e5
Agile Deployer <agile-deployer@users.noreply.github.com>
2021-06-14 14:54:16 +0100

    Create ConnectDBServer.sh

---
+++ providerscripts/utilities/ConnectDBServer.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+#################################################################################
+# Author: Peter Winter
+# Date :  9/4/2016
+# Description: Connect to the Database Server machine (linux)
+#################################################################################
+# License Agreement:
+# This file is part of The Agile Deployment Toolkit.
+# The Agile Deployment Toolkit is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# The Agile Deployment Toolkit is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with The Agile Deployment Toolkit.  If not, see <http://www.gnu.org/licenses/>.
+#####################################################################################
+#####################################################################################
+#set -x
+
+SERVER_USER="`/bin/ls ${HOME}/.ssh/SERVERUSER:* | /usr/bin/awk -F':' '{print $NF}'`"
+SSH_PORT="`/bin/ls ${HOME}/.ssh/SSH_PORT:* | /usr/bin/awk -F':' '{print $NF}'`"
+ALGORITHM="`/bin/ls ${HOME}/.ssh/ALGORITHM:* | /usr/bin/awk -F':' '{print $NF}'`"
+HOST="`/bin/ls ${HOME}/config/databaseip`"
+
+/usr/bin/ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i ${HOME}/.ssh/id_${ALGORITHM}_AGILE_DEPLOYMENT_BUILD_KEY ${SERVER_USER}@${HOST} -p ${SSH_PORT} 
