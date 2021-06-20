8a55a12a0e2c560f63ea21963a95010d9e5ac77d
Agile Deployer <agile-deployer@users.noreply.github.com>
2021-06-20 15:35:24 +0100

    Update ExtractConfigValue.sh

--- providerscripts/utilities/ExtractConfigValue.sh
+++ providerscripts/utilities/ExtractConfigValue.sh
@@ -1,1 +1,5 @@
-/bin/grep "${1}:" ${HOME}/.ssh/webserver_configuration_settings.dat | /usr/bin/awk -F':' '{print $NF}'
+
+if ( [ "${1}" != "" ] )
+then
+    /bin/grep "${1}:" ${HOME}/.ssh/webserver_configuration_settings.dat | /usr/bin/awk -F':' '{print $NF}'
+fi
