efd39c2279459a81089dd616165e7c3e148372f6
Agile Deployer <agile-deployer@users.noreply.github.com>
2021-06-20 17:07:31 +0100

    Update CheckConfigValue.sh

--- providerscripts/utilities/CheckConfigValue.sh
+++ providerscripts/utilities/CheckConfigValue.sh
@@ -1,3 +1,5 @@
+export HOME="`/bin/cat /home/homedir.dat`"
+
 if ( [ "`/bin/grep "${1}:" ${HOME}/.ssh/webserver_configuration_settings.dat | /usr/bin/awk -F':' '{print $NF}'`" = "${2}" ] )
 then
     /bin/echo "0"
