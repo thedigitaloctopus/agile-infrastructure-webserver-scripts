cfeeb39261cd5d3f088123fc0c94c1fd8cf267b2
Agile Deployer <agile-deployer@users.noreply.github.com>
2021-06-20 17:08:42 +0100

    Update ExtractConfigValues.sh

--- providerscripts/utilities/ExtractConfigValues.sh
+++ providerscripts/utilities/ExtractConfigValues.sh
@@ -1,3 +1,5 @@
+export HOME="`/bin/cat /home/homedir.dat`"
+
 if ( [ "${1}" != "" ] )
 then
     /bin/grep "${1}:" ${HOME}/.ssh/webserver_configuration_settings.dat | /usr/bin/awk -F':' '{$1=""; print $0}'
