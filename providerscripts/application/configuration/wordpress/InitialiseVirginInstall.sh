674060f0e5e0a46bff96a7452efd33354546104a
Agile Deployer <agile-deployer@users.noreply.github.com>
2021-06-21 19:56:31 +0100

    Update InitialiseVirginInstall.sh

--- providerscripts/application/configuration/wordpress/InitialiseVirginInstall.sh
+++ providerscripts/application/configuration/wordpress/InitialiseVirginInstall.sh
@@ -121,7 +121,7 @@ fi
 #Note, please check crontab and you will find that there this script is called every minute which is what I mean
 #by aggressive.
 
-if ( [ "`${HOME}/providerscripts/utilities/CheckConfigValue.sh BUILDARCHIVECHOICE:virgin`" = "1" ] )
+if ( [ "`${HOME}/providerscripts/utilities/CheckConfigValue.sh BUILDARCHIVECHOICE:virgin`" = "0" ] )
 then
     exit
 fi
