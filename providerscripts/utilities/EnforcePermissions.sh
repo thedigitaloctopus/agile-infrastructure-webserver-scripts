8e0040e360351b4d08dff029be954a400658a41d
Agile Deployer <agile-deployer@users.noreply.github.com>
2021-06-21 14:34:40 +0100

    Update EnforcePermissions.sh

--- providerscripts/utilities/EnforcePermissions.sh
+++ providerscripts/utilities/EnforcePermissions.sh
@@ -21,7 +21,9 @@
 ##########################################################################################
 #set -x
 
-directoriestomiss="`/bin/ls ${HOME}/.ssh/DIRECTORIESTOMOUNT:* | /bin/sed 's/.*DIRECTORIESTOMOUNT://g' | /bin/sed 's/:/ /g' | /bin/sed 's/\./\//g' | /usr/bin/tr '\n' ' ' | /bin/sed 's/  / /g'`"
+#directoriestomiss="`/bin/ls ${HOME}/.ssh/DIRECTORIESTOMOUNT:* | /bin/sed 's/.*DIRECTORIESTOMOUNT://g' | /bin/sed 's/:/ /g' | /bin/sed 's/\./\//g' | /usr/bin/tr '\n' ' ' | /bin/sed 's/  / /g'`"
+directoriestomiss="`${HOME}/providerscripts/utilities/ExtractConfigValues.sh 'DIRECTORIESTOMOUNT' 'stripped' | /bin/sed 's/\./\//g' | /usr/bin/tr '\n' ' ' | /bin/sed 's/  / /g'`"
+
 
 CMD="/usr/bin/find /var/www/html/ -not -path "
 
