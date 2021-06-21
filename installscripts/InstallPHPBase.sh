6f0ad014c1199e12793fd611f3607f2ea3fd346d
Agile Deployer <agile-deployer@users.noreply.github.com>
2021-06-21 15:21:30 +0100

    Update InstallPHPBase.sh

--- installscripts/InstallPHPBase.sh
+++ installscripts/InstallPHPBase.sh
@@ -18,7 +18,7 @@
 # along with The Agile Deployment Toolkit.  If not, see <http://www.gnu.org/licenses/>.
 #######################################################################################################
 #######################################################################################################
-if ( [ ! -f ${HOME}/.ssh/APPLICATIONLANGUAGE:PHP ] )
+if ( [ ! "`${HOME}/providerscripts/utilities/CheckConfigValue.sh APPLICATIONLANGUAGE:PHP`" ] )
 then
     exit
 fi
