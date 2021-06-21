ee019a4e9275a7a5e93d4337ef7d5e3e1db98fae
Agile Deployer <agile-deployer@users.noreply.github.com>
2021-06-21 17:03:06 +0100

    Update SetupAssetsStore.sh

--- providerscripts/datastore/SetupAssetsStore.sh
+++ providerscripts/datastore/SetupAssetsStore.sh
@@ -23,7 +23,7 @@
 
 WEBSITE_URL="`${HOME}/providerscripts/utilities/ExtractConfigValue.sh 'WEBSITEURL'`"
 
-if ( [ -f ${HOME}/.ssh/PERSISTASSETSTOCLOUD:0 ] && [ ! -f ${HOME}/runtime/S3BUCKETSET ] )
+if ( [ "`${HOME}/providerscripts/utilities/CheckConfigValue.sh PERSISTASSETSTOCLOUD:0`" = "1" ] && [ ! -f ${HOME}/runtime/S3BUCKETSET ] )
 then
     domainspecifier="`/bin/echo ${WEBSITE_URL} | /usr/bin/awk -F'.' '{ for(i = 1; i <= NF; i++) { print $i; } }' | /usr/bin/cut -c1-3 | /usr/bin/tr '\n' '-' | /bin/sed 's/-//g'`"
     /usr/bin/s3cmd mb s3://${domainspecifier}
