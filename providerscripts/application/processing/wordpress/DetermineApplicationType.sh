42e61eec24f166f9669d544a0abb02510718dda8
Agile Deployer <agile-deployer@users.noreply.github.com>
2021-06-21 21:15:43 +0100

    Update DetermineApplicationType.sh

--- providerscripts/application/processing/wordpress/DetermineApplicationType.sh
+++ providerscripts/application/processing/wordpress/DetermineApplicationType.sh
@@ -23,5 +23,6 @@
 
 if ( [ -f /var/www/html/wp-login.php ] )
 then
-    /bin/touch ${HOME}/.ssh/APPLICATION:wordpress
+   # /bin/touch ${HOME}/.ssh/APPLICATION:wordpress
+    ${HOME}/providerscripts/utilities/StoreConfigValue.sh "APPLICATION" "wordpress"
 fi
