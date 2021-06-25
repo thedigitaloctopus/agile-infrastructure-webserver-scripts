f841bf557561a3ff8d90692e94fce80bbd571a8f
Agile Deployer <agile-deployer@users.noreply.github.com>
2021-06-25 10:51:31 +0100

    Create CheckBuildStyle.sh

---
+++ providerscripts/utilities/CheckBuildStyle.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+export HOME="`/bin/cat /home/homedir.dat`"
+
+key_value="`/bin/grep "${1}" ${HOME}/.ssh/buildstylesscp.dat`"
+
+if ( [ "${key_value}" != "" ] )
+then
+    /bin/echo "1"
+else
+    /bin/echo "0" 
+fi
