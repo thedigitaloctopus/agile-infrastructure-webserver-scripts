a57cbef2b3d57bc83ff48e34f4a5390308012edd
Agile Deployer <agile-deployer@users.noreply.github.com>
2021-07-02 16:21:22 +0100

    Update StoreConfigValue.sh

--- providerscripts/utilities/StoreConfigValue.sh
+++ providerscripts/utilities/StoreConfigValue.sh
@@ -4,9 +4,10 @@ export HOME="`/bin/cat /home/homedir.dat`"
 
 if ( [ "${1}" != "" ] && [ "${2}" != "" ] )
 then
-    if ( [ "${3}" != "append" ] )
-    then
-       /bin/sed -i "/^${1}:/d" ${HOME}/.ssh/webserver_configuration_settings.dat
-    fi
-    /bin/echo "${1}:${2}" >> ${HOME}/.ssh/webserver_configuration_settings.dat
+    /bin/sed -i "/^${1}:/d" ${HOME}/.ssh/autoscaler_configuration_settings.dat
+    /bin/echo "${1}:${2}" >> ${HOME}/.ssh/autoscaler_configuration_settings.dat
+elif ( [ "${1}" != "" ] && [ "${2}" = "" ] )
+then
+    /bin/sed -i "/^${1}$/d" ${HOME}/.ssh/autoscaler_configuration_settings.dat
+    /bin/echo "${1}" >> ${HOME}/.ssh/autoscaler_configuration_settings.dat
 fi
