4682e154aa01d4eab8d33a867f47cdea5d703c6a
Agile Deployer <agile-deployer@users.noreply.github.com>
2021-07-30 14:25:24 +0100

    Update BuildApacheFromSource.sh

--- installscripts/apache/BuildApacheFromSource.sh
+++ installscripts/apache/BuildApacheFromSource.sh
@@ -1,4 +1,8 @@
-cd /usr/local/src/httpd-2.4.43/srclib/
+apache_download_link="`/usr/bin/curl http://httpd.apache.org/download.cgi | /bin/grep "Source" | /bin/grep "tar.gz" | /bin/sed 's/.*https/https/g' | /bin/sed 's/".*//g'`"
+apache_version="`/bin/echo ${apache_download_link} | /bin/sed 's/.*\///g' | sed 's/.tar.gz//g'`"
+/usr/bin/wget -O- ${apache_download_link} | /bin/tar -zxf -
+
+cd /usr/local/src/${apache_vesion}/srclib/
 wget https://downloads.apache.org/apr/apr-1.7.0.tar.gz
 tar -xzf apr-1.7.0.tar.gz
 mv apr-1.7.0 apr
@@ -8,12 +12,13 @@ wget https://downloads.apache.org/apr/apr-util-1.6.1.tar.gz
 tar -xzf apr-util-1.6.1.tar.gz
 mv apr-util-1.6.1 apr-util
 
-cd /usr/local/src/
-wget https://downloads.apache.org//httpd/httpd-2.4.43.tar.gz
-tar -xzf httpd-2.4.43.tar.gz
-apt install build-essential libssl-dev libexpat-dev libpcre2-dev libapr1-dev libaprutil1-dev
+pcre_latest_version="`/usr/bin/curl 'https://ftp.pcre.org/pub/pcre/' | /bin/egrep -o 'pcre-[0-9]+\.[0-9]+' | /bin/sed 's/pcre-//g' | /usr/bin/sort --version-sort | /usr/bin/uniq | /usr/bin/tail -1`"
 
-cd /usr/local/src/httpd-2.4.43
-./configure
+/usr/bin/wget -O- https://ftp.pcre.org/pub/pcre/pcre-${pcre_latest_version}.tar.gz | /bin/tar -zxf -
+
+cd /usr/local/src/${apache_version}
+
+./configure --prefix=/usr/local/apache2 --sysconfdir=/etc/apache2  --with-mpm=prefork --enable-http2 --enable-ssl --enable-so --enable-rewrite --enable-mods-static="reallyall" --enable-mods-shared="reallyall"
+
 make
 make install
