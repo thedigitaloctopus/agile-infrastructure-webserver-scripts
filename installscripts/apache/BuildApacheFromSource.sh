428ca9818465b0c98ff8716713619763521793b8
Agile Deployer <agile-deployer@users.noreply.github.com>
2021-07-31 20:28:23 +0100

    Update BuildApacheFromSource.sh

--- installscripts/apache/BuildApacheFromSource.sh
+++ installscripts/apache/BuildApacheFromSource.sh
@@ -1,10 +1,38 @@
-set -x
+#!/bin/sh
+######################################################################################################
+# Description: This script will install the apache webserver from source
+# The advantages of installing from source is that you can use the latest available version of the software
+# the apt repositories tend to be quite some way behind the newest releases.
+# Also, you have more control over what modules are used. You can add and remove modules as you need to 
+# by changing the configuration parameters
+# The disadvantage is that the build process will take longer to complete
+# Author: Peter Winter
+# Date: 04/06/2021
+#######################################################################################################
+# License Agreement:
+# This file is part of The Agile Deployment Toolkit.
+# The Agile Deployment Toolkit is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# The Agile Deployment Toolkit is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with The Agile Deployment Toolkit.  If not, see <http://www.gnu.org/licenses/>.
+#######################################################################################################
+#######################################################################################################
+#set -x
 
+#Install needed libraries
 /usr/bin/apt-get -qq -y  install libnghttp2-dev libssl-dev  build-essential autoconf libtool openssl apache2-dev
+
+
 cd /usr/local/src
 
+#Download and build PCRE
 pcre_latest_version="`/usr/bin/curl 'https://ftp.pcre.org/pub/pcre/' | /bin/egrep -o 'pcre-[0-9]+\.[0-9]+' | /bin/sed 's/pcre-//g' | /usr/bin/sort --version-sort | /usr/bin/uniq | /usr/bin/tail -1`"
-
 /usr/bin/wget -O- https://ftp.pcre.org/pub/pcre/pcre-${pcre_latest_version}.tar.gz | /bin/tar -zxf -
 
 cd /usr/local/src/pcre*
@@ -15,6 +43,7 @@ cd /usr/local/src/pcre*
 
 cd /usr/local/src
 
+#Download and build libexpat
 /usr/bin/git clone https://github.com/libexpat/libexpat.git
 
 cd /usr/local/src/libexpat/expat
@@ -25,21 +54,18 @@ cd /usr/local/src/libexpat/expat
 
 cd /usr/local/src
 
+#Download and build openssl
+
 /usr/bin/git clone https://github.com/openssl/openssl.git
-
 cd /usr/local/src/openssl
-
 ./Configure --prefix=/usr/local/ssl --openssldir=/usr/local/ssl '-Wl,--enable-new-dtags,-rpath,$(LIBRPATH)'
-
 /usr/bin/make
 /usr/bin/make install
 
 
 cd /usr/local/src
 
-apache_download_link="`/usr/bin/curl http://httpd.apache.org/download.cgi | /bin/grep "Source" | /bin/grep "tar.gz" | /bin/sed 's/.*https/https/g' | /bin/sed 's/".*//g'`"
-
-/usr/bin/wget -O- ${apache_download_link} | /bin/tar -zxf -
+#Download about build apr
 
 apr_latest_version="`/usr/bin/curl http://apr.apache.org/download.cgi | /bin/grep "apr1" | /bin/sed 's/.*APR //g' | /usr/bin/awk '{print $1}'`"
 apr_download_link="https://mirrors.ukfast.co.uk/sites/ftp.apache.org/apr/apr-${apr_latest_version}.tar.gz"
@@ -53,6 +79,8 @@ make
 make install
 
 cd /usr/local/src
+
+#Download and build apr-util
 
 apr_util_download_link="`/usr/bin/curl http://apr.apache.org/download.cgi | /bin/grep 'apr-util' | /bin/grep 'tar.gz\"' | /bin/sed 's/.*https/https/g' | /bin/sed 's/".*//g' | /bin/sed '/^$/d'`"
 
@@ -72,23 +100,18 @@ cd ..
 /bin/mkdir /usr/local/src/`/bin/ls /usr/local/src/ | /bin/grep httpd`/srclib/apr
 /bin/mv /usr/local/src/apr-*/* `/bin/ls /usr/local/src/ | /bin/grep httpd`/srclib/apr
 
+#Download and build apache
+apache_download_link="`/usr/bin/curl http://httpd.apache.org/download.cgi | /bin/grep "Source" | /bin/grep "tar.gz" | /bin/sed 's/.*https/https/g' | /bin/sed 's/".*//g'`"
+/usr/bin/wget -O- ${apache_download_link} | /bin/tar -zxf -
+
 cd httpd-*
 
-#./configure --enable-ssl --enable-so --with-mpm=event --with-included-apr --prefix=/usr/local/apache2 --with-pcre=/usr/local/pcre --enable-mods-shared="reallyall" --enable-mpms-shared="all"
-#./configure --enable-ssl --enable-so --with-mpm=event --with-included-apr --enable-proxy --enable-ssl --enable-rewrite --with-mpm=worker --prefix=/usr/local/apache2 --with-pcre=/usr/local/pcre --enable-mpms-shared="all" --enable-mods-shared="all ssl cache proxy http2 authn_alias mem_cache file_cache charset_lite dav_lock disk_cache"
-#./configure --prefix=/etc/apache2 --with-pcre=/usr/local/pcre --with-apxs2=/usr/bin/apxs --with-mpm=prefork --enable-http2 --enable-ssl --enable-so --with-included-apr --enable-rewrite --enable-mods-static="reallyall" --enable-mods-shared="reallyall"
 /bin/mkdir /usr/local/apache2
 /bin/mkdir /etc/apache2
 /bin/mkdir /etc/apache2/conf
 /bin/mkdir /etc/apache2/mods-available
 /bin/mkdir /etc/apache2/conf-available
 /bin/mkdir /etc/apache2/sites-available
-
-###################################################################
-#git clone https://github.com/apache/httpd.git
-#./buildconf --with-apr=../apr-1.7.0 --with-apr-util=../apr-util-1.6
-
-#####################################
 
 
 ./configure --prefix=/usr/local/apache2 --sysconfdir=/etc/apache2 --with-pcre=/usr/local/pcre --with-apr=/usr/local/apr --with-apxs2=/usr/bin/apxs --with-ssl=/usr/local/ssl -with-mpm=prefork --enable-http2 --enable-ssl --enable-so --enable-rewrite --enable-mods-static="reallyall" --enable-mods-shared="reallyall"
@@ -97,8 +120,7 @@ cd httpd-*
 
 /usr/bin/make install
 
-###INSTALL MOD SECURITY NEEDS WORK
-###################################################################
+#Download and build and configure mod security
 dir="`/usr/bin/pwd`"
 /usr/bin/git clone https://github.com/ssdeep-project/ssdeep
 cd ssdeep/
@@ -129,7 +151,7 @@ cd ModSecurity-apache
 /usr/bin/make install
 cd ${dir}
 
-
+#Install modsecurity rules
 /usr/bin/git clone https://github.com/SpiderLabs/ModSecurity 
 /bin/mkdir /etc/apache2/modsecurity.d 
 /bin/cp ./ModSecurity/modsecurity.conf-recommended /etc/apache2/modsecurity.d/modsecurity.conf 
@@ -154,25 +176,10 @@ WEBSITE_NAME="`${HOME}/providerscripts/utilities/ExtractConfigValue.sh 'WEBSITED
 /bin/sed -i '/:443/a modsecurity on\nmodsecurity_rules_file /etc/apache2/modsecurity.d/modsec_rules.conf' /etc/apache2/sites-available/${WEBSITE_NAME}
 
 
-
-#/usr/bin/wget https://github.com/SpiderLabs/owasp-modsecurity-crs/tarball/master
-#/bin/mv master master.tar.gz
-#/bin/tar xvfz master.tar.gz
-#/bin/cp -R SpiderLabs-owasp-modsecurity-crs-*/ /usr/local/apache2/conf/crs/
-#cd /usr/local/apache2/conf/crs/
-#/bin/mv modsecurity_crs_10_setup.conf.example modsecurity_crs_10_setup.conf
-#/bin/ln -s /usr/local/apache2/conf/crs/modsecurity_crs_10_setup.conf activated_rules/
-#for f in `ls base_rules/` ; do ln -s /usr/local/apache2/conf/crs/base_rules/$f activated_rules/$f ; done
-#for f in `ls optional_rules/` ; do ln -s /usr/local/apache2/conf/crs/optional_rules/$f activated_rules/$f ; done
-#/bin/mkdir /etc/modsecurity
-#cd ${dir}
-#/bin/cp ModSecurity/modsecurity.conf-recommended /etc/modsecurity/modsecurity.conf
-#/bin/cp ModSecurity/unicode.mapping /etc/modsecurity/
-#/bin/sed -i "s/DetectionOnly/On/g" /etc/modsecurity/modsecurity.conf
-####################################################################
-
 /bin/cp /usr/local/apache2/conf/mime.types /etc/apache2/conf
 
+
+#Put code to start apache after a reboot
 /bin/echo "#!/bin/bash
 /bin/mkdir /var/run/apache2
 /bin/chown www-data.www-data /var/run/apache2
@@ -210,6 +217,7 @@ GuessMainPID=no
 [Install]
 WantedBy=multi-user.target" > /etc/systemd/system/rc-local.service
 
+#Install configuration values for apache
 /bin/cp ${HOME}/installscripts/apache/httpd.conf.sample /etc/apache2/httpd.conf
 /bin/cp ${HOME}/installscripts/apache/envvars.sample /etc/apache2/envvars
 /bin/cp ${HOME}/installscripts/apache/magic.sample /etc/apache2/magic
@@ -222,6 +230,7 @@ WantedBy=multi-user.target" > /etc/systemd/system/rc-local.service
 /bin/mkdir /var/log/apache2
 /bin/chown www-data.www-data /var/log/apache2
 
+#Set required modules for loading
 /bin/echo "LoadModule unixd_module  /usr/local/apache2/modules/mod_unixd.so
 LoadModule authz_core_module  /usr/local/apache2/modules/mod_authz_core.so
 LoadModule log_config_module /usr/local/apache2/modules/mod_log_config.so
@@ -245,11 +254,7 @@ LoadModule session_cookie_module /usr/local/apache2/modules/mod_session_cookie.s
 LoadModule session_crypto_module /usr/local/apache2/modules/mod_session_crypto.so" > /etc/apache2/httpd.conf.$$
 
 /bin/cat /etc/apache2/httpd.conf >> /etc/apache2/httpd.conf.$$
-
 /bin/mv /etc/apache2/httpd.conf.$$ /etc/apache2/httpd.conf
-
-#/bin/mv /etc/apache2/httpd.conf /etc/apache2/httpd.conf.orig
-#/bin/mv /etc/apache2/apache2.conf /etc/apache2/httpd.conf
 /bin/sed -i "s/^#ServerRoot.*/ServerRoot \"\/etc\/apache2\"/g" /etc/apache2/httpd.conf
 
 /bin/mv /etc/apache2/conf/magic.conf /etc/apache2/conf/magic.orig
@@ -259,8 +264,9 @@ LoadModule session_crypto_module /usr/local/apache2/modules/mod_session_crypto.s
 /bin/mv /etc/apache2/conf/ports.conf /etc/apache2/conf/ports.conf.orig
 /bin/ln -s /etc/apache2/ports.conf /etc/apache2/conf/ports.conf
 
-/bin/ln -s /etc/apache2/conf-available/remoteip.conf /etc/apache2/conf-enabled/remoteip.conf
+#/bin/ln -s /etc/apache2/conf-available/remoteip.conf /etc/apache2/conf-enabled/remoteip.conf
 
+#Setup to pass PHP requests to FCGI
 if ( [ "`${HOME}/providerscripts/utilities/CheckConfigValue.sh APPLICATIONLANGUAGE:PHP`" = "1" ] )
 then
    /bin/echo "ProxyPassMatch ^/(.*\.php(/.*)?)$ fcgi://127.0.0.1:9000/var/www/html/\$1" >> /etc/apache2/httpd.conf
