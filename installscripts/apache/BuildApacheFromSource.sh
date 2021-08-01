33e43e41e737e4e52d258400db7c10b32c7eeea4
Agile Deployer <agile-deployer@users.noreply.github.com>
2021-08-02 00:00:19 +0100

    Update BuildApacheFromSource.sh

--- installscripts/apache/BuildApacheFromSource.sh
+++ installscripts/apache/BuildApacheFromSource.sh
@@ -28,7 +28,7 @@
 #Install needed libraries
 /usr/bin/apt-get -qq -y  install libnghttp2-dev  build-essential autoconf libtool software-properties-common
 #apache2-dev libssl-dev openssl
-/usr/bin/add-apt-repository ppa:maxmind/ppa
+/usr/bin/add-apt-repository -y ppa:maxmind/ppa
 /usr/bin/apt -qq -y update
 /usr/bin/apt -qq -y install libmaxminddb0 libmaxminddb-dev mmdb-bin
 
