e9f324203db6b8acd212a0ac9e32e5c090780e78
Agile Deployer <agile-deployer@users.noreply.github.com>
2021-08-02 15:49:12 +0100

    Update BuildApacheFromSource.sh

--- installscripts/apache/BuildApacheFromSource.sh
+++ installscripts/apache/BuildApacheFromSource.sh
@@ -172,7 +172,7 @@ EOL
 
 cd ${dir}
 
-/bin/rm /etc/apache2/modsecurity.d/owasp-crs/rules/REQUEST-910-IP-REPUTATION.conf #Requires max mind
+#/bin/rm /etc/apache2/modsecurity.d/owasp-crs/rules/REQUEST-910-IP-REPUTATION.conf #Requires max mind
 
 WEBSITE_NAME="`${HOME}/providerscripts/utilities/ExtractConfigValue.sh 'WEBSITEDISPLAYNAME'`"
 /bin/sed -i '/:443/a modsecurity on\nmodsecurity_rules_file /etc/apache2/modsecurity.d/modsec_rules.conf' /etc/apache2/sites-available/${WEBSITE_NAME}
