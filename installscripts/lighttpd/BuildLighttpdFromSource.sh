2f78afa08eec809316e3350a09e633dc1ab6c0a6
Agile Deployer <agile-deployer@users.noreply.github.com>
2021-08-06 13:27:46 +0100

    Create BuildLighttpdFromSource.sh

---
+++ installscripts/lighttpd/BuildLighttpdFromSource.sh
@@ -0,0 +1,38 @@
+/usr/bin/apt-get install -qq -y autoconf automake libtool m4 pkg-config 
+/usr/bin/apt install -qq -y bzip2 libgeoip-dev gnutls-bin libmaxminddb-dev libxml2 libmariadb-dev libpq-dev zlib1g-dev libssl-dev libpcre3-dev
+
+/usr/bin/git clone https://github.com/lighttpd/lighttpd1.4.git
+cd lighttp*
+./autogen.sh
+./configure -C --with-geoip --with-gnutls --with-maxminddb --with-pgsql --with-mysql --with-openssl --with-pcre
+/usr/bin/make
+/usr/bin/make install 
+
+#Put code to start apache after a reboot
+/bin/echo "#!/bin/bash
+/usr/local/sbin/lighttpd
+/bin/sleep 10
+lighties=\"\`/usr/bin/ps -ef | /bin/grep lighttp | /bin/grep -v grep | /usr/bin/wc -l\`\"
+while ( [ \"\${lighties}\" = \"0\" ] )
+do
+    /usr/local/sbin/lighttpd
+    lighties=\"\`/usr/bin/ps -ef | /bin/grep lighttp | /bin/grep -v grep | /usr/bin/wc -l\`\"
+    /bin/sleep 10
+done
+exit 0" > /etc/rc.local
+
+/bin/chmod +x /etc/rc.local
+
+/bin/echo "[Unit]
+Description=/etc/rc.local Compatibility
+Documentation=man:systemd-rc-local-generator(8)
+ConditionFileIsExecutable=/etc/rc.local
+After=network.target
+[Service]
+Type=forking
+ExecStart=/etc/rc.local start
+TimeoutSec=0
+RemainAfterExit=yes
+GuessMainPID=no
+[Install]
+WantedBy=multi-user.target" > /etc/systemd/system/rc-local.service
