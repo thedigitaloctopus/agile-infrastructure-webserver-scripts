c33bb057637a1307d442a391dea837a42e528b18
Agile Deployer <agile-deployer@users.noreply.github.com>
2021-08-13 22:25:56 +0100

    Update PutToDatastore.sh

--- providerscripts/datastore/PutToDatastore.sh
+++ providerscripts/datastore/PutToDatastore.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-##############################################################################################
+#####################################################################################
 # Author: Peter Winter
 # Date :  9/4/2016
 # Description: Put files into a bucket in the datastore
-##############################################################################################
+#####################################################################################
 # License Agreement:
 # This file is part of The Agile Deployment Toolkit.
 # The Agile Deployment Toolkit is free software: you can redistribute it and/or modify
@@ -16,15 +16,15 @@
 # GNU General Public License for more details.
 # You should have received a copy of the GNU General Public License
 # along with The Agile Deployment Toolkit.  If not, see <http://www.gnu.org/licenses/>.
-#######################################################################################
-#######################################################################################
-set -x
+######################################################################################
+######################################################################################
+#set -x
 
 datastore_provider="$1"
 file_to_put="$2"
 datastore_to_put_in="$3"
 
-if ( [ "${datastore_provider}" = "amazonS3" ] || [ "${datastore_provider}" = "digitalocean" ] || [ "${datastore_provider}" = "exoscale" ] || [ "${datastore_provider}" = "linode" ] || [ "${datastore_provider}" = "vultr" ] )
+if ( [ "${datastore_provider}" = "amazonS3" ] || [ "${datastore_provider}" = "digitalocean" ] || [ "${datastore_provider}" = "exoscale" ] ||  [ "${datastore_provider}" = "linode" ] || [ "${datastore_provider}" = "vultr" ]  )
 then
     /usr/bin/s3cmd --force --recursive --multipart-chunk-size-mb=5 put ${file_to_put} s3://${datastore_to_put_in}
     file="`/bin/echo ${file_to_put} | /usr/bin/awk -F'/' '{print $NF}'`"
