6b84634ab1ceb9da035e563a2bb9057d12a8e6fe
Agile Deployer <agile-deployer@users.noreply.github.com>
2021-08-16 20:22:19 +0100

    Update InstallApache.sh

--- providerscripts/webserver/configuration/InstallApache.sh
+++ providerscripts/webserver/configuration/InstallApache.sh
@@ -49,7 +49,7 @@ LimitRequestFields 50
 NameVirtualHost *:80
 <VirtualHost *:80>
    ServerName ${website_url}
-   Redirect permanent / https://${website_url/
+   Redirect permanent / https://${website_url}/
 </VirtualHost>
 
 <VirtualHost _default_:443>
