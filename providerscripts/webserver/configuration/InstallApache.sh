3e76d2bc3acaa19f12496a72c7619778b2d2cca6
Agile Deployer <agile-deployer@users.noreply.github.com>
2021-08-18 17:49:18 +0100

    Update InstallApache.sh

--- providerscripts/webserver/configuration/InstallApache.sh
+++ providerscripts/webserver/configuration/InstallApache.sh
@@ -92,9 +92,11 @@ LimitRequestFields 50
             ExpiresByType text/css \"access plus 8 days\"
 </IfModule>" > /etc/apache2/sites-available/${WEBSITE_NAME}
 
-if ( [ -f /etc/apache2/mods-available/security2.conf ] )
+if ( [ -f /usr/local/apache2/modules/mod_security3.so ] )
 then
     /bin/echo "SecRuleEngine On" >> /etc/apache2/sites-available/${WEBSITE_NAME}
+    /bin/echo "modsecurity on" >> /etc/apache2/sites-available/${WEBSITE_NAME}
+    /bin/echo "modsecurity_rules_file /etc/apache2/modsecurity.d/modsec_rules.conf" >> /etc/apache2/sites-available/${WEBSITE_NAME}
 fi
 
 /bin/echo "
