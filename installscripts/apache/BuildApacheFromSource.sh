8ae0ae3694dcda498be2c465990a2c6a0286a75a
Agile Deployer <agile-deployer@users.noreply.github.com>
2021-08-21 16:45:49 +0100

    Update BuildApacheFromSource.sh

--- installscripts/apache/BuildApacheFromSource.sh
+++ installscripts/apache/BuildApacheFromSource.sh
@@ -205,13 +205,17 @@ fi
 
 if ( [ "${3}" = "modevasive" ] )
 then            
-            /bin/mkdir -p /usr/lib/apache2/modules
+          #  /bin/mkdir -p /usr/lib/apache2/modules
             
-            /usr/bin/git clone https://github.com/jzdziarski/mod_evasive.git
-            /usr/bin/apt -qq -y install apache2-dev
-            cd mod_evasive
-            /bin/sed -i "s/remote_ip/client_ip/g" mod_evasive20.c
-            /usr/bin/apxs2 -i -c mod_evasive20.c
+          #  /usr/bin/git clone https://github.com/jzdziarski/mod_evasive.git
+          #  /usr/bin/apt -qq -y install apache2-dev
+          #  cd mod_evasive
+          #  /bin/sed -i "s/remote_ip/client_ip/g" mod_evasive20.c
+          #  /usr/bin/apxs2 -i -c mod_evasive20.c
+          
+            /usr/bin/apt-get -qq -y install apache2-utils
+            /usr/bin/apt-get -qq -y install libapache2-mod-evasive
+            
             cd ..
             /bin/cp ${HOME}/installscripts/apache/mod_evasive.sample /etc/apache2/mods-available/evasive.conf
             /usr/bin/ln -s /etc/apache2/mods-available/evasive.conf /etc/apache2/mods-enabled/evasive.conf
