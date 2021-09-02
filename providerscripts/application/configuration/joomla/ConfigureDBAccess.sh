96767aa9cd91767800377751cd92b768e2100c8f
Agile Deployer <agile-deployer@users.noreply.github.com>
2021-09-02 17:10:00 +0100

    Update ConfigureDBAccess.sh

--- providerscripts/application/configuration/joomla/ConfigureDBAccess.sh
+++ providerscripts/application/configuration/joomla/ConfigureDBAccess.sh
@@ -118,9 +118,6 @@ else
     /bin/cp /var/www/html/configuration.php.default ${HOME}/runtime/joomla_configuration.php
 fi
 
-#This file enables us to detect which configuration file has been set from elsewhere if we are not sure
-#/bin/touch ${HOME}/.ssh/CONFIGFILE:joomla_configuration.php
-
 #Set the credentials that we need
 DB_PORT="`${HOME}/providerscripts/utilities/ExtractConfigValue.sh 'DBPORT'`"
 
