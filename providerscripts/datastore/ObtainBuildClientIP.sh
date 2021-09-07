bcc0f96663ee2994707d3735f26d9718f456303c
Agile Deployer <agile-deployer@users.noreply.github.com>
2021-09-07 15:54:24 +0100

    Update ObtainBuildClientIP.sh

--- providerscripts/datastore/ObtainBuildClientIP.sh
+++ providerscripts/datastore/ObtainBuildClientIP.sh
@@ -1,2 +1,3 @@
 /bin/mkdir /tmp/BUILDCLIENTIP
+BUILD_IDENTIFIER="`${HOME}/providerscripts/utilities/ExtractConfigValue.sh 'BUILDIDENTIFIER'`"
 /usr/bin/s3cmd get s3://adt-${BUILD_IDENTIFIER}/* /tmp/BUILDCLIENTIP
