662e02119e6ecea2d6b1e2599f84135c5365d87b
Agile Deployer <agile-deployer@users.noreply.github.com>
2021-09-11 11:07:04 +0100

    Update ObtainBuildClientIP.sh

--- providerscripts/datastore/ObtainBuildClientIP.sh
+++ providerscripts/datastore/ObtainBuildClientIP.sh
@@ -5,5 +5,8 @@ fi
 /bin/mkdir /tmp/BUILDCLIENTIP
 BUILD_IDENTIFIER="`${HOME}/providerscripts/utilities/ExtractConfigValue.sh 'BUILDIDENTIFIER'`"
 /usr/bin/s3cmd get s3://adt-${BUILD_IDENTIFIER}/* /tmp/BUILDCLIENTIP
-BUILD_CLIENT_IP="`/bin/ls /tmp/BUILDCLIENTIP/* | /usr/bin/awk -F'/' '{print $NF}'`"
-${HOME}/providerscripts/utilities/StoreConfigValue.sh "BUILDCLIENTIP" "${BUILD_CLIENT_IP}"
+BUILD_CLIENT_IP="`/bin/ls /tmp/BUILDCLIENTIP/* | /usr/bin/awk -F'/' '{print $NF}' | /bin/grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}'`"
+if ( [ "${BUILD_CLIENT_IP}" != "" ] )
+then
+    ${HOME}/providerscripts/utilities/StoreConfigValue.sh "BUILDCLIENTIP" "${BUILD_CLIENT_IP}"
+fi 
