3e37a67d0cd58f29dc4632a94637f65bce58d288
Agile Deployer <agile-deployer@users.noreply.github.com>
2021-09-13 11:45:56 +0100

    Update ObtainBuildClientIP.sh

--- providerscripts/datastore/ObtainBuildClientIP.sh
+++ providerscripts/datastore/ObtainBuildClientIP.sh
@@ -7,8 +7,22 @@ if ( [ "`/bin/ls ${HOME}/runtime/BUILDCLIENTIP/*`" != "" ] && [ -f ${HOME}/runti
 then
     exit
 fi
+
+uptime="`/usr/bin/uptime | /usr/bin/awk -F ',' ' {print $1} ' | /usr/bin/awk ' {print $3} ' | /usr/bin/awk -F ':' ' {hrs=$1; min=$2; print
+ hrs*60 + min} '`"
+ 
+ if ( [ "${uptime}" -gt "15" ] )
+ then
+     exit
+ fi
+ 
 BUILD_IDENTIFIER="`${HOME}/providerscripts/utilities/ExtractConfigValue.sh 'BUILDIDENTIFIER'`"
 /usr/bin/s3cmd get s3://adt-${BUILD_IDENTIFIER}/* ${HOME}/runtime/BUILDCLIENTIP
 BUILD_CLIENT_IP="`/bin/ls ${HOME}/runtime/BUILDCLIENTIP/* | /usr/bin/awk -F'/' '{print $NF}' | /bin/grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}'`"
-${HOME}/providerscripts/utilities/StoreConfigValue.sh "BUILDCLIENTIP" "${BUILD_CLIENT_IP}"
-/bin/touch ${HOME}/runtime/BUILDCLIENTUPDATED 
+OLD_BUILD_CLIENT_IP="`${HOME}/providerscripts/utilities/ExtractConfigValue.sh 'BUILDCLIENTIP'`"
+
+if ( [ "${OLD_BUILD_CLIENT_IP}" != "${BUILD_CLIENT_IP}" ] )
+then
+    ${HOME}/providerscripts/utilities/StoreConfigValue.sh "BUILDCLIENTIP" "${BUILD_CLIENT_IP}"  
+    /bin/touch ${HOME}/runtime/BUILDCLIENTUPDATED
+fi
