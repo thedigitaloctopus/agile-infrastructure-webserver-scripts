67c73aed28afaca84e1386b50f2be7483a1ffd9b
Agile Deployer <agile-deployer@users.noreply.github.com>
2021-09-24 19:31:24 +0100

    Update GatewayGuardian.sh

--- security/GatewayGuardian.sh
+++ security/GatewayGuardian.sh
@@ -1,8 +1,9 @@
+#!/bin/sh
 
 BUILD_IDENTIFIER="`${HOME}/providerscripts/utilities/ExtractConfigValue.sh 'BUILDIDENTIFIER'`"
 
 
-if ( [ -f ${HOME}/config/credentials/GATEWAY_GUARDIAN_UPDATED ] )
+if ( [ -f ${HOME}/config/credentials/GATEWAY_GUARDIAN_UPDATED ] || [ ! -f /var/www/html/administrator/.htpasswd ] )
 then
     /usr/bin/s3cmd get s3://gatewayguardian-${BUILD_IDENTIFIER}/htpasswd
     /bin/mv htpasswd /var/www/html/administrator/.htpasswd
