2209f88f53f2052ea728c6fb7a919352df045307
Agile Deployer <agile-deployer@users.noreply.github.com>
2021-09-25 12:14:07 +0100

    Update GatewayGuardian.sh

--- security/GatewayGuardian.sh
+++ security/GatewayGuardian.sh
@@ -2,12 +2,26 @@
 
 BUILD_IDENTIFIER="`${HOME}/providerscripts/utilities/ExtractConfigValue.sh 'BUILDIDENTIFIER'`"
 
-
-if ( [ -f ${HOME}/config/credentials/GATEWAY_GUARDIAN_UPDATED ] || [ ! -f /var/www/html/administrator/.htpasswd ] )
+if ( [ "`${HOME}/providerscripts/utilities/CheckConfigValue.sh APPLICATION:joomla`" = "1" ] )
 then
-    /usr/bin/s3cmd get s3://gatewayguardian-${BUILD_IDENTIFIER}/htpasswd
-    /bin/mv htpasswd /var/www/html/administrator/.htpasswd
-    /bin/chown www-data.www-data /var/www/html/administrator/.htpasswd
-    /bin/sleep 40
-    /bin/rm ${HOME}/config/credentials/GATEWAY_GUARDIAN_UPDATED
+    if ( [ -f ${HOME}/config/credentials/GATEWAY_GUARDIAN_UPDATED ] || [ ! -f /var/www/html/administrator/.htpasswd ] )
+    then
+        /usr/bin/s3cmd get s3://gatewayguardian-${BUILD_IDENTIFIER}/htpasswd
+        /bin/mv htpasswd /var/www/html/administrator/.htpasswd
+        /bin/chown www-data.www-data /var/www/html/administrator/.htpasswd
+        /bin/sleep 40
+        /bin/rm ${HOME}/config/credentials/GATEWAY_GUARDIAN_UPDATED
+    fi
+fi
+
+if ( [ "`${HOME}/providerscripts/utilities/CheckConfigValue.sh APPLICATION:wordpress`" = "1" ] )
+then
+    if ( [ -f ${HOME}/config/credentials/GATEWAY_GUARDIAN_UPDATED ] || [ ! -f /var/www/html/wp-admin/.htpasswd ] )
+    then
+        /usr/bin/s3cmd get s3://gatewayguardian-${BUILD_IDENTIFIER}/htpasswd
+        /bin/mv htpasswd /var/www/html/wp-admin/.htpasswd
+        /bin/chown www-data.www-data /var/www/html/wp-admin/.htpasswd
+        /bin/sleep 40
+        /bin/rm ${HOME}/config/credentials/GATEWAY_GUARDIAN_UPDATED
+    fi
 fi
