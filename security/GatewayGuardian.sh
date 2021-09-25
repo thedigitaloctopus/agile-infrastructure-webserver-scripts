172c04af6c9e9faba9212d057f3ea0d7f8943b2f
Agile Deployer <agile-deployer@users.noreply.github.com>
2021-09-25 20:47:50 +0100

    Update GatewayGuardian.sh

--- security/GatewayGuardian.sh
+++ security/GatewayGuardian.sh
@@ -26,6 +26,18 @@ then
     fi
 fi
 
+if ( [ "`${HOME}/providerscripts/utilities/CheckConfigValue.sh APPLICATION:drupal`" = "1" ] )
+then
+    if ( [ -f ${HOME}/config/credentials/GATEWAY_GUARDIAN_UPDATED ] || [ ! -f /var/www/html/.htpasswd ] )
+    then
+        /usr/bin/s3cmd get s3://gatewayguardian-${BUILD_IDENTIFIER}/htpasswd
+        /bin/mv htpasswd /var/www/html/.htpasswd
+        /bin/chown www-data.www-data /var/www/html/.htpasswd
+        /bin/sleep 40
+        /bin/rm ${HOME}/config/credentials/GATEWAY_GUARDIAN_UPDATED
+    fi
+fi
+
 if ( [ "`${HOME}/providerscripts/utilities/CheckConfigValue.sh APPLICATION:moodle`" = "1" ] )
 then
     if ( [ -f ${HOME}/config/credentials/GATEWAY_GUARDIAN_UPDATED ] || [ ! -f /var/www/html/moodle/.htpasswd ] )
