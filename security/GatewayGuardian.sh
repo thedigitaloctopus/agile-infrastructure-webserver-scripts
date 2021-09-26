56daa622a825cefcf6214ab5f08d472158384df0
Agile Deployer <agile-deployer@users.noreply.github.com>
2021-09-26 22:53:34 +0100

    Update GatewayGuardian.sh

--- security/GatewayGuardian.sh
+++ security/GatewayGuardian.sh
@@ -9,7 +9,7 @@ fi
 
 if ( [ "`${HOME}/providerscripts/utilities/CheckConfigValue.sh APPLICATION:joomla`" = "1" ] )
 then
-    if ( [ -f ${HOME}/config/credentials/GATEWAY_GUARDIAN_UPDATED ] || [ ! -f /etc/basicauth/.htpasswd] )
+    if ( [ -f ${HOME}/config/credentials/GATEWAY_GUARDIAN_UPDATED ] || [ ! -f /etc/basicauth/.htpasswd ] )
     then
         /usr/bin/s3cmd get s3://gatewayguardian-${BUILD_IDENTIFIER}/htpasswd
         /bin/mv htpasswd /etc/basicauth/.htpasswd
