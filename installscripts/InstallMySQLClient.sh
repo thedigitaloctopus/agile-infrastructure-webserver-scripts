38531b5b75502a990766cddce03cc9c879e7db70
Agile Deployer <agile-deployer@users.noreply.github.com>
2021-10-04 19:50:30 +0100

    Update InstallMySQLClient.sh

--- installscripts/InstallMySQLClient.sh
+++ installscripts/InstallMySQLClient.sh
@@ -36,18 +36,26 @@ DB_P="`/bin/sed '2q;d' ${HOME}/credentials/shit`"
 
 if ( [ "${BUILDOS}" = "ubuntu" ] )
 then
-    /usr/bin/wget https://dev.mysql.com/get/mysql-apt-config_0.8.13-1_all.deb
-    DEBIAN_FRONTEND=noninteractive /usr/bin/dpkg -i mysql-apt-config*
-    /bin/rm mysql-apt-config*
+    #/usr/bin/wget https://dev.mysql.com/get/mysql-apt-config_0.8.13-1_all.deb
+    #DEBIAN_FRONTEND=noninteractive /usr/bin/dpkg -i mysql-apt-config*
+    #/bin/rm mysql-apt-config*
+    mysql_apt_config="`/usr/bin/wget -O- https://dev.mysql.com/downloads/repo/apt/ | /bin/grep -o mysql-apt-config.* | /usr/bin/head -1 | /bin/sed 's/deb.*/deb/g'`"
+    /usr/bin/wget https://dev.mysql.com/get/${mysql_apt_config} 
+    DEBIAN_FRONTEND=noninteractive /usr/bin/dpkg -i ${mysql_apt_config}
+    /bin/rm ${mysql_apt_config}
     ${HOME}/installscripts/Update.sh ${BUILDOS}
     DEBIAN_FRONTEND=noninteractive apt-get -qq -y install mysql-client
 fi
 
 if ( [ "${BUILDOS}" = "debian" ] )
 then
-    /usr/bin/wget https://dev.mysql.com/get/mysql-apt-config_0.8.13-1_all.deb
-    DEBIAN_FRONTEND=noninteractive /usr/bin/dpkg -i mysql-apt-config*
-    /bin/rm mysql-apt-config*
+    #/usr/bin/wget https://dev.mysql.com/get/mysql-apt-config_0.8.13-1_all.deb
+    #DEBIAN_FRONTEND=noninteractive /usr/bin/dpkg -i mysql-apt-config*
+    #/bin/rm mysql-apt-config*
+    mysql_apt_config="`/usr/bin/wget -O- https://dev.mysql.com/downloads/repo/apt/ | /bin/grep -o mysql-apt-config.* | /usr/bin/head -1 | /bin/sed 's/deb.*/deb/g'`"
+    /usr/bin/wget https://dev.mysql.com/get/${mysql_apt_config} 
+    DEBIAN_FRONTEND=noninteractive /usr/bin/dpkg -i ${mysql_apt_config}
+    /bin/rm ${mysql_apt_config}
     ${HOME}/installscripts/Update.sh ${BUILDOS}
     DEBIAN_FRONTEND=noninteractive apt-get -qq -y install mysql-client
 fi
