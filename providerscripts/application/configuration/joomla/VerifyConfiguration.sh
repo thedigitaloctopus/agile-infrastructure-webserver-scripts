988b599793ec0af43db10037d49112f4de669281
Agile Deployer <agile-deployer@users.noreply.github.com>
2021-10-08 16:38:53 +0100

    Update VerifyConfiguration.sh

--- providerscripts/application/configuration/joomla/VerifyConfiguration.sh
+++ providerscripts/application/configuration/joomla/VerifyConfiguration.sh
@@ -29,12 +29,12 @@ then
     /bin/rm ${HOME}/config/GLOBAL_CONFIG_UPDATE 
 fi
 
-/usr/bin/wget --timeout=10 --tries=3 --spider --no-check-certificate https://localhost/index.php
+#/usr/bin/wget --timeout=10 --tries=3 --spider --no-check-certificate https://localhost/index.php
 
-if ( [ "$?" != "0" ] || [ "`/usr/bin/diff /var/www/html/configuration.php ${HOME}/config/joomla_configuration.php`" != "" ]  )
-then
-    /bin/rm ${HOME}/runtime/CONFIG_VERIFIED
-fi
+#if ( [ "$?" != "0" ] || [ "`/usr/bin/diff /var/www/html/configuration.php ${HOME}/config/joomla_configuration.php`" != "" ]  )
+#then
+#    /bin/rm ${HOME}/runtime/CONFIG_VERIFIED
+#fi
 
 if ( [ -f ${HOME}/runtime/CONFIG_VERIFIED ] )
 then
