b05fab8983b3602aa7ddc1b25b6ba36a2e31a257
Agile Deployer <agile-deployer@users.noreply.github.com>
2021-10-09 16:30:43 +0100

    Update AllowGatewayBypass.sh

--- providerscripts/webserver/configuration/AllowGatewayBypass.sh
+++ providerscripts/webserver/configuration/AllowGatewayBypass.sh
@@ -10,19 +10,24 @@ fi
 
 WEBSITE_NAME="`${HOME}/providerscripts/utilities/ExtractConfigValue.sh 'WEBSITEDISPLAYNAME'`"
 
-if ( [ "`${HOME}/providerscripts/utilities/CheckConfigValue.sh WEBSERVERCHOICE:APACHE`" = "1" ] )
+if ( [ "`${HOME}/providerscripts/utilities/CheckConfigValue.sh WEBSERVERCHOICE:NGINX`" = "1" ] )
 then
+    /bin/echo "satisfy any;" >> /etc/nginx/sites-available/bypass_snippet.dat
     for ip in "`/bin/ls ${HOME}/config/autoscalerip | /usr/bin/tr '\n' ' '`"
     do
-        /bin/echo "                 <RequireAny>" >> /etc/apache2/sites-available/bypass_snippet.dat
-        /bin/echo "                      Require ip ${ip}" >> /etc/apache2/sites-available/bypass_snippet.dat
-        /bin/echo "                      Require valid-user" >> /etc/apache2/sites-available/bypass_snippet.dat
-        /bin/echo "                 </RequireAny>" >> /etc/apache2/sites-available/bypass_snippet.dat
+        /bin/echo "                      allow ${ip};" >> /etc/nginx/sites-available/bypass_snippet.dat
     done
+    
+    for ip in "`/bin/ls ${HOME}/config/autoscalerpublicip | /usr/bin/tr '\n' ' '`"
+    do
+        /bin/echo "                      allow ${ip};" >> /etc/nginx/sites-available/bypass_snippet.dat
+    done
+    
+    /bin/echo "deny all;" >> /etc/nginx/sites-available/bypass_snippet.dat
 
     if ( [ -f /etc/apache2/sites-available/bypass_snippet.dat ] )
     then
-        /bin/sed -i -e '/####BYPASS####/{r /etc/apache2/sites-available/bypass_snippet.dat' -e 'd}' /etc/apache2/sites-available/${WEBSITE_NAME} 
+        /bin/sed -i -e '/####BYPASS####/{r /etc/nginx/sites-available/bypass_snippet.dat' -e 'd}' /etc/nginx/sites-available/${WEBSITE_NAME} 
     fi
 
     /bin/touch ${HOME}/runtime/BYPASS_PROCESSED
