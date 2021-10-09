6072870eb610dfeb9a73258bbee86f617addba09
Agile Deployer <agile-deployer@users.noreply.github.com>
2021-10-09 16:23:46 +0100

    Update AllowGatewayBypass.sh

--- providerscripts/webserver/configuration/AllowGatewayBypass.sh
+++ providerscripts/webserver/configuration/AllowGatewayBypass.sh
@@ -10,18 +10,40 @@ fi
 
 WEBSITE_NAME="`${HOME}/providerscripts/utilities/ExtractConfigValue.sh 'WEBSITEDISPLAYNAME'`"
 
-for ip in "`/bin/ls ${HOME}/config/autoscalerip | /usr/bin/tr '\n' ' '`"
-do
-    /bin/echo "                 <RequireAny>" >> /etc/apache2/sites-available/bypass_snippet.dat
-    /bin/echo "                      Require ip ${ip}" >> /etc/apache2/sites-available/bypass_snippet.dat
-    /bin/echo "                      Require valid-user" >> /etc/apache2/sites-available/bypass_snippet.dat
-    /bin/echo "                 </RequireAny>" >> /etc/apache2/sites-available/bypass_snippet.dat
-done
-
-if ( [ -f /etc/apache2/sites-available/bypass_snippet.dat ] )
+if ( [ "`${HOME}/providerscripts/utilities/CheckConfigValue.sh WEBSERVERCHOICE:APACHE`" = "1" ] )
 then
-    /bin/sed -i -e '/####BYPASS####/{r /etc/apache2/sites-available/bypass_snippet.dat' -e 'd}' /etc/apache2/sites-available/${WEBSITE_NAME} 
+    for ip in "`/bin/ls ${HOME}/config/autoscalerip | /usr/bin/tr '\n' ' '`"
+    do
+        /bin/echo "                 <RequireAny>" >> /etc/apache2/sites-available/bypass_snippet.dat
+        /bin/echo "                      Require ip ${ip}" >> /etc/apache2/sites-available/bypass_snippet.dat
+        /bin/echo "                      Require valid-user" >> /etc/apache2/sites-available/bypass_snippet.dat
+        /bin/echo "                 </RequireAny>" >> /etc/apache2/sites-available/bypass_snippet.dat
+    done
+
+    if ( [ -f /etc/apache2/sites-available/bypass_snippet.dat ] )
+    then
+        /bin/sed -i -e '/####BYPASS####/{r /etc/apache2/sites-available/bypass_snippet.dat' -e 'd}' /etc/apache2/sites-available/${WEBSITE_NAME} 
+    fi
+
+    /bin/touch ${HOME}/runtime/BYPASS_PROCESSED
+    /bin/rm /etc/apache2/sites-available/bypass_snippet.dat
 fi
 
-/bin/touch ${HOME}/runtime/BYPASS_PROCESSED
-/bin/rm /etc/apache2/sites-available/bypass_snippet.dat
+if ( [ "`${HOME}/providerscripts/utilities/CheckConfigValue.sh WEBSERVERCHOICE:APACHE`" = "1" ] )
+then
+    for ip in "`/bin/ls ${HOME}/config/autoscalerip | /usr/bin/tr '\n' ' '`"
+    do
+        /bin/echo "                 <RequireAny>" >> /etc/apache2/sites-available/bypass_snippet.dat
+        /bin/echo "                      Require ip ${ip}" >> /etc/apache2/sites-available/bypass_snippet.dat
+        /bin/echo "                      Require valid-user" >> /etc/apache2/sites-available/bypass_snippet.dat
+        /bin/echo "                 </RequireAny>" >> /etc/apache2/sites-available/bypass_snippet.dat
+    done
+
+    if ( [ -f /etc/apache2/sites-available/bypass_snippet.dat ] )
+    then
+        /bin/sed -i -e '/####BYPASS####/{r /etc/apache2/sites-available/bypass_snippet.dat' -e 'd}' /etc/apache2/sites-available/${WEBSITE_NAME} 
+    fi
+
+    /bin/touch ${HOME}/runtime/BYPASS_PROCESSED
+    /bin/rm /etc/apache2/sites-available/bypass_snippet.dat
+fi
