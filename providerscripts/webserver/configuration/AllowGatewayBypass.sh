1712c037a53cc6d402e8804c396526f3dd80f95b
Agile Deployer <agile-deployer@users.noreply.github.com>
2021-10-09 14:13:25 +0100

    Update AllowGatewayBypass.sh

--- providerscripts/webserver/configuration/AllowGatewayBypass.sh
+++ providerscripts/webserver/configuration/AllowGatewayBypass.sh
@@ -1,11 +1,26 @@
-    if ( [ "`${HOME}/providerscripts/utilities/CheckConfigValue.sh GATEWAYGUARDIAN:1`" = "1" ] )
-    then
+if ( [ -f ${HOME}/runtime/BYPASS_PROCESSED ] )
+then
+    exit
+fi
 
-        for ip in "`/bin/ls ${HOME}/config/autoscalerip | /usr/bin/tr '\n' ' '`"
-        do
-           /bin/echo "<RequireAny>" >> /etc/apache2/sites-available/${WEBSITE_NAME}
-           /bin/echo "    Require ip ${ip}" >> /etc/apache2/sites-available/${WEBSITE_NAME}
-           /bin/echo "    Require valid-user" >> /etc/apache2/sites-available/${WEBSITE_NAME}
-           /bin/echo "</RequireAny>" >> /etc/apache2/sites-available/${WEBSITE_NAME}
-        done
-    fi
+if ( [ "`/bin/mount | /bin/grep ${HOME}/config`" = "" ] )
+then
+   exit
+fi
+
+WEBSITE_NAME="`${HOME}/providerscripts/utilities/ExtractConfigValue.sh 'WEBSITEDISPLAYNAME'`"
+
+for ip in "`/bin/ls ${HOME}/config/autoscalerip | /usr/bin/tr '\n' ' '`"
+do
+    /bin/echo "<RequireAny>" >> /etc/apache2/sites-available/bypass_snippet.dat
+    /bin/echo "    Require ip ${ip}" >> /etc/apache2/sites-available/bypass_snippet.dat
+    /bin/echo "    Require valid-user" >> /etc/apache2/sites-available/bypass_snippet.dat
+    /bin/echo "</RequireAny>" >> /etc/apache2/sites-available/bypass_snippet.dat
+done
+
+if ( [ -f /etc/apache2/sites-available/bypass_snippet.dat ] )
+then
+    /bin/sed -i -e '/####BYPASS####/{r /etc/apache2/sites-available/bypass_snippet.dat' -e 'd}' /etc/apache2/sites-available/${WEBSITE_NAME} 
+fi
+
+/bin/touch ${HOME}/runtime/BYPASS_PROCESSED
