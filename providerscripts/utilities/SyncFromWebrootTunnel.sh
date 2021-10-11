6ebdd898e21cacf2a5e269dd14cc5dc9c02452db
Agile Deployer <agile-deployer@users.noreply.github.com>
2021-10-11 19:09:24 +0100

    Update SyncFromWebrootTunnel.sh

--- providerscripts/utilities/SyncFromWebrootTunnel.sh
+++ providerscripts/utilities/SyncFromWebrootTunnel.sh
@@ -25,17 +25,17 @@ then
     exit
 fi
 
-if ( [ -f ${HOME}/config/ENABLESYNCTUNNEL ] )
-then
-    /bin/touch ${HOME}/config/SYNCTUNNELENABLED
-    /bin/rm ${HOME}/config/ENABLESYNCTUNNEL
-    /bin/rm -r ${HOME}/config/webrootsynctunnel/*
-fi
+#if ( [ -f ${HOME}/config/ENABLESYNCTUNNEL ] )
+#then
+#    /bin/touch ${HOME}/config/SYNCTUNNELENABLED
+#    /bin/rm ${HOME}/config/ENABLESYNCTUNNEL
+#    /bin/rm -r ${HOME}/config/webrootsynctunnel/*
+#fi
 
-if ( [ ! -f ${HOME}/config/SYNCTUNNELENABLED ] )
-then
-    exit
-fi
+#if ( [ ! -f ${HOME}/config/SYNCTUNNELENABLED ] )
+#then
+#    exit
+#fi
 
 ip="`${HOME}/providerscripts/utilities/ExtractConfigValue.sh 'MYPUBLICIP'`"
 
