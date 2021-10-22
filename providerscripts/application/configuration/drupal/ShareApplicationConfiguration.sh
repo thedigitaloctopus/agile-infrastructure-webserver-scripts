27b32b12409a440295b0ed4e21fd1163f3a9f627
Agile Deployer <agile-deployer@users.noreply.github.com>
2021-10-22 22:16:34 +0100

    Update ShareApplicationConfiguration.sh

--- providerscripts/application/configuration/drupal/ShareApplicationConfiguration.sh
+++ providerscripts/application/configuration/drupal/ShareApplicationConfiguration.sh
@@ -63,7 +63,7 @@ changed=""
 
 if ( [ "${updated}" = "1" ] )
 then
-    if ( [ "`/usr/bin/find ${HOME}/config/drupal_settings.php -mmin -1`" != "" ] )
+    if ( [ "`/usr/bin/find ${HOME}/config/drupal_settings.php -mmin -2`" != "" ] )
     then
         #This check is needed so we don't accidentally update the config file and push it to all our webservers
         #We have to explicitly create the GLOBAL_CONFIG_UPDATE file to do that. 
@@ -72,11 +72,11 @@ then
             changed="config"
         fi
     fi
-    if ( [ "`/usr/bin/find /var/www/html/sites/default/settings.php -mmin -1`" != "" ] )
+    if ( [ "`/usr/bin/find /var/www/html/sites/default/settings.php -mmin -2`" != "" ] )
     then
         changed="main"
     fi
-    if ( [ "`/usr/bin/find ${HOME}/runtime/drupal_settings.php -mmin -1`" != "" ] )
+    if ( [ "`/usr/bin/find ${HOME}/runtime/drupal_settings.php -mmin -2`" != "" ] )
     then
         changed="runtime"
     fi
