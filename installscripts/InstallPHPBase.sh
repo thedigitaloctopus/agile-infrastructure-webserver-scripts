9a1641c9c7bde54e8559c21eb1bfc9e6abf56783
Agile Deployer <agile-deployer@users.noreply.github.com>
2021-10-29 19:57:05 +0100

    Update InstallPHPBase.sh

--- installscripts/InstallPHPBase.sh
+++ installscripts/InstallPHPBase.sh
@@ -46,11 +46,11 @@ then
             /usr/bin/apt-get -qq -y autoremove
         fi
         
-        extensions="`${HOME}/providerscripts/utilities/ExtractConfigValues.sh "PHP" "stripped" | /bin/sed 's/:/ /g'`"
+        modules="`${HOME}/providerscripts/utilities/ExtractBuildStyleValues.sh "PHP" "stripped" | /bin/sed 's/:/ /g'`"
     
-        for extension in ${extensions}
+        for module in ${modules}
         do
-            /usr/bin/apt-get -qq -y install php${PHP_VERSION}-${extension}
+            /usr/bin/apt-get -qq -y install php${PHP_VERSION}-${module}
         done
           
        /usr/bin/apt-get -qq -y install php${PHP_VERSION}-fpm php${PHP_VERSION}-cli php${PHP_VERSION}-gmp  php${PHP_VERSION}-xmlrpc php${PHP_VERSION}-soap php${PHP_VERSION}-dev php${PHP_VERSION}-mysqli php${PHP_VERSION}-phpdbg php${PHP_VERSION}-mbstring php${PHP_VERSION}-gd php${PHP_VERSION}-imap php${PHP_VERSION}-ldap php${PHP_VERSION}-pgsql php${PHP_VERSION}-pspell php${PHP_VERSION}-tidy php${PHP_VERSION}-intl php${PHP_VERSION}-gd php${PHP_VERSION}-curl php${PHP_VERSION}-zip php${PHP_VERSION}-xml php${PHP_VERSION}-imagick php${PHP_VERSION}-ssh2 php${PHP_VERSION}-sqlite3
@@ -80,11 +80,11 @@ then
         /usr/bin/apt-get -qq -y autoremove
     fi
     
-    extensions="`${HOME}/providerscripts/utilities/ExtractConfigValues.sh "PHP" "stripped" | /bin/sed 's/:/ /g'`"
+    modules="`${HOME}/providerscripts/utilities/ExtractBuildStyleValues.sh "PHP" "stripped" | /bin/sed 's/:/ /g'`"
     
-    for extension in ${extensions}
+    for module in ${modules}
     do
-        /usr/bin/apt-get -qq -y install php${PHP_VERSION}-${extension}
+        /usr/bin/apt-get -qq -y install php${PHP_VERSION}-${module}
     done
 
     #/usr/bin/apt-get -qq -y install php${PHP_VERSION}-fpm php${PHP_VERSION}-cli php${PHP_VERSION}-gmp php${PHP_VERSION}-xmlrpc php${PHP_VERSION}-soap php${PHP_VERSION}-dev php${PHP_VERSION}-mysqli php${PHP_VERSION}-phpdbg php${PHP_VERSION}-mbstring php${PHP_VERSION}-gd php${PHP_VERSION}-imap php${PHP_VERSION}-ldap php${PHP_VERSION}-pgsql php${PHP_VERSION}-pspell php${PHP_VERSION}-tidy php${PHP_VERSION}-intl php${PHP_VERSION}-gd php${PHP_VERSION}-curl php${PHP_VERSION}-zip php${PHP_VERSION}-xml php${PHP_VERSION}-imagick php${PHP_VERSION}-ssh2 php${PHP_VERSION}-sqlite3
