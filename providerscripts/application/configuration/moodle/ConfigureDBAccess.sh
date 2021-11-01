8f365856141f3cd6a9a0609a09525f4574c77975
Agile Deployer <agile-deployer@users.noreply.github.com>
2021-11-01 20:40:23 +0000

    Update ConfigureDBAccess.sh

--- providerscripts/application/configuration/moodle/ConfigureDBAccess.sh
+++ providerscripts/application/configuration/moodle/ConfigureDBAccess.sh
@@ -131,29 +131,29 @@ if ( [ "`${HOME}/providerscripts/utilities/CheckConfigValue.sh DATABASEINSTALLAT
 then
     if ( [ "`${HOME}/providerscripts/utilities/CheckConfigValue.sh DATABASEDBaaSINSTALLATIONTYPE:Maria`" = "1" ] )
     then
-        /bin/sed -i "/->dbtype /c\    \$CFG->dbtype    = \"mysqli\";" /var/www/html/moodle/config.php
-        /bin/echo "MySQL" > /var/www/html/dbe.dat
+        /bin/sed -i "/->dbtype /c\    \$CFG->dbtype    = \"mariadb\";" /var/www/html/moodle/config.php
+        /bin/echo "For your information, this website uses Maria DB" > /var/www/html/dbe.dat
     elif ( [ "`${HOME}/providerscripts/utilities/CheckConfigValue.sh DATABASEDBaaSINSTALLATIONTYPE:MySQL`" = "1" ] )
     then
         /bin/sed -i "/->dbtype /c\    \$CFG->dbtype    = \"mysqli\";" /var/www/html/moodle/config.php
-        /bin/echo "MySQL" > /var/www/html/dbe.dat
+        /bin/echo "For your information, this website uses MySQL" > /var/www/html/dbe.dat
     elif ( [ "`${HOME}/providerscripts/utilities/CheckConfigValue.sh DATABASEDBaaSINSTALLATIONTYPE:Postgres`" = "1" ] )
     then
         /bin/sed -i "/->dbtype /c\    \$CFG->dbtype    = \"pgsql\";" /var/www/html/moodle/config.php
-        /bin/echo "Postgres" > /var/www/html/dbe.dat
+        /bin/echo "For your information, this website uses Postgres" > /var/www/html/dbe.dat
     fi
 elif ( [ "`${HOME}/providerscripts/utilities/CheckConfigValue.sh DATABASEINSTALLATIONTYPE:Maria`" = "1" ] )
 then
     /bin/sed -i "/->dbtype /c\    \$CFG->dbtype    = \"mariadb\";" /var/www/html/moodle/config.php
-    /bin/echo "MySQL" > /var/www/html/dbe.dat
+    /bin/echo "For your information, this website uses MariaDB" > /var/www/html/dbe.dat
 elif ( [ "`${HOME}/providerscripts/utilities/CheckConfigValue.sh DATABASEINSTALLATIONTYPE:MySQL`" = "1" ] )
 then
     /bin/sed -i "/->dbtype /c\    \$CFG->dbtype    = \"mysqli\";" /var/www/html/moodle/config.php 
-    /bin/echo "MySQL" > /var/www/html/dbe.dat
+    /bin/echo "For your information, this website uses MySQL" > /var/www/html/dbe.dat
 elif ( [ "`${HOME}/providerscripts/utilities/CheckConfigValue.sh DATABASEINSTALLATIONTYPE:Postgres`" = "1" ] )
 then
     /bin/sed -i "/->dbtype /c\    \$CFG->dbtype    = \"pgsql\";" /var/www/html/moodle/config.php
-    /bin/echo "Postgres" > /var/www/html/dbe.dat
+    /bin/echo "For your information, this website uses Postgres" > /var/www/html/dbe.dat
 fi
 
 if ( [ -f /var/www/html/moodle/config.php ] && [ "`/bin/grep "${name}" /var/www/html/moodle/config.php`" = "" ] )
