b4b82bf44ef8aa607499fa7581cbbbd4d9d2d3ac
Agile Deployer <agile-deployer@users.noreply.github.com>
2021-11-03 16:55:18 +0000

    Update BuildModsecurityForRepo.sh

--- installscripts/nginx/BuildModsecurityForRepo.sh
+++ installscripts/nginx/BuildModsecurityForRepo.sh
@@ -1,4 +1,7 @@
 /usr/bin/apt-get -qq -y install bison build-essential ca-certificates curl dh-autoreconf doxygen flex gawk git iputils-ping libcurl4-gnutls-dev libexpat1-dev libgeoip-dev liblmdb-dev libpcre3-dev libpcre++-dev libssl-dev libtool libxml2 libxml2-dev libyajl-dev locales lua5.3-dev pkg-config wget zlib1g-dev zlibc libgd-dev libxslt-dev
+/usr/bin/apt-get install -y -qq libcurl4-openssl-dev
+/usr/bin/apt-get install -y -qq libxml2-dev
+/usr/bin/apt-get install -y -qq libpcre3-dev
 
 cd /opt
 
