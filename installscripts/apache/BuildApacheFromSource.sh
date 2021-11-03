45a397829ac710d633aa5615d3e16c3f4c4b4386
Agile Deployer <agile-deployer@users.noreply.github.com>
2021-11-03 20:58:59 +0000

    Update BuildApacheFromSource.sh

--- installscripts/apache/BuildApacheFromSource.sh
+++ installscripts/apache/BuildApacheFromSource.sh
@@ -154,7 +154,8 @@ fi
 
 if ( [ "${3}" = "modevasive" ] )
 then                      
-    DEBIAN_FRONTEND=noninteractive /usr/bin/apt-get -qq -y install apache2-utils
+    export DEBIAN_FRONTEND=noninteractive 
+    /usr/bin/apt-get -qq -y install apache2-utils
     /usr/bin/apt-get -qq -y install libapache2-mod-evasive
             
     /bin/mkdir /var/log/mod_evasive 
