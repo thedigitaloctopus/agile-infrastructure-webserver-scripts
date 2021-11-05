63a5fd1dacb04d871cdd5b8706336be62e4e0561
Agile Deployer <agile-deployer@users.noreply.github.com>
2021-11-05 14:13:24 +0000

    Update RefreshNetworking.sh

--- providerscripts/utilities/RefreshNetworking.sh
+++ providerscripts/utilities/RefreshNetworking.sh
@@ -39,18 +39,21 @@ then
         if ( [ "${BUILDOSVERSION}" = "20.04" ] )
 	then
 	    ip="`${HOME}/providerscripts/utilities/ExtractConfigValue.sh 'MYIP'`"
-	    /bin/sed -i "s/addresses.*/addresses: [${ip}\/16]/" /etc/netplan/10-ens7.yaml
-            if ( [ -f /etc/netplan/10-ens3.yaml ] )
-            then
-                /bin/echo "network:
-  version: 2
-  renderer: networkd
-  ethernets:
-    ens3:
-      mtu: 1450
-      dhcp4: yes
-      addresses: [${ip}/16]" > /etc/netplan/10-ens3.yaml
-             fi
+	    #/bin/sed -i "s/addresses.*/addresses: [${ip}\/16]/" /etc/netplan/10-ens7.yaml
+	    
+	    /bin/sed -i "s/addresses.*/addresses: [${ip}\/16]/" /etc/netplan/10-enp6s0.yaml
+
+          #  if ( [ -f /etc/netplan/10-ens3.yaml ] )
+          #  then
+          #      /bin/echo "network:
+ # version: 2
+ # renderer: networkd
+ # ethernets:
+ #   ens3:
+ #     mtu: 1450
+ #     dhcp4: yes
+ #     addresses: [${ip}/16]" > /etc/netplan/10-ens3.yaml
+ #            fi
             /usr/sbin/netplan apply
 	fi
     fi
