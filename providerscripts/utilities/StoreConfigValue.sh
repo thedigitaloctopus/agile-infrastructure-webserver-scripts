794e6720e57977ecffe1b753739f2e779083b4f4
Agile Deployer <agile-deployer@users.noreply.github.com>
2021-11-06 23:50:17 +0000

    Update StoreConfigValue.sh

--- providerscripts/utilities/StoreConfigValue.sh
+++ providerscripts/utilities/StoreConfigValue.sh
@@ -2,18 +2,16 @@
 
 export HOME="`/bin/cat /home/homedir.dat`"
 
-#if ( [ "${1}" != "" ] && [ "${2}" != "" ] )
-#then
-#    /bin/sed -i "/^${1}:/d" ${HOME}/.ssh/webserver_configuration_settings.dat
-#    /bin/sed -i "\$ a\ ${1}:${2}" ${HOME}/.ssh/webserver_configuration_settings.dat 
-#    /bin/sed -i "s/^ //g" ${HOME}/.ssh/webserver_configuration_settings.dat 
-#   # /bin/echo "${1}:${2}" >> ${HOME}/.ssh/webserver_configuration_settings.dat
-#elif ( [ "${1}" != "" ] && [ "${2}" = "" ] )
-#then
-#    /bin/sed -i "/^${1}$/d" ${HOME}/.ssh/webserver_configuration_settings.dat
-#    /bin/sed -i "\$ a\ ${1}" ${HOME}/.ssh/webserver_configuration_settings.dat 
-#    /bin/sed -i "s/^ //g" ${HOME}/.ssh/webserver_configuration_settings.dat 
-#  #  /bin/echo "${1}" >> ${HOME}/.ssh/webserver_configuration_settings.dat
-#fi
-
-/bin/echo "CALLED"
+if ( [ "${1}" != "" ] && [ "${2}" != "" ] )
+then
+    /bin/sed -i "/^${1}:/d" ${HOME}/.ssh/webserver_configuration_settings.dat
+    /bin/sed -i "\$ a\ ${1}:${2}" ${HOME}/.ssh/webserver_configuration_settings.dat 
+    /bin/sed -i "s/^ //g" ${HOME}/.ssh/webserver_configuration_settings.dat 
+   # /bin/echo "${1}:${2}" >> ${HOME}/.ssh/webserver_configuration_settings.dat
+elif ( [ "${1}" != "" ] && [ "${2}" = "" ] )
+then
+    /bin/sed -i "/^${1}$/d" ${HOME}/.ssh/webserver_configuration_settings.dat
+    /bin/sed -i "\$ a\ ${1}" ${HOME}/.ssh/webserver_configuration_settings.dat 
+    /bin/sed -i "s/^ //g" ${HOME}/.ssh/webserver_configuration_settings.dat 
+  #  /bin/echo "${1}" >> ${HOME}/.ssh/webserver_configuration_settings.dat
+fi
