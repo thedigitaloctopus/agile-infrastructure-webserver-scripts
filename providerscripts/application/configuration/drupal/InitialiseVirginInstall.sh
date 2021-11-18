2acfb053f997139e7955dd64ba246b63fe01e388
Agile Deployer <agile-deployer@users.noreply.github.com>
2021-11-18 22:29:16 +0000

    Update InitialiseVirginInstall.sh

--- providerscripts/application/configuration/drupal/InitialiseVirginInstall.sh
+++ providerscripts/application/configuration/drupal/InitialiseVirginInstall.sh
@@ -65,8 +65,11 @@ DB_PORT="`${HOME}/providerscripts/utilities/ExtractConfigValue.sh 'DBPORT'`"
 if ( [ ! -f /var/www/html/dbp.dat ] )
 then
     prefix="`/bin/cat /dev/urandom | /usr/bin/tr -dc a-z | /usr/bin/head -c${1:-6};echo;`"
-    ${HOME}/providerscripts/utilities/StoreConfigValue.sh "DBPREFIX" "${prefix}"
-    /bin/echo "${prefix}" > /var/www/html/dbp.dat
+    if ( [ "${prefix}" != "" ] )
+    then
+        ${HOME}/providerscripts/utilities/StoreConfigValue.sh "DBPREFIX" "${prefix}"
+        /bin/echo "${prefix}" > /var/www/html/dbp.dat
+    fi
 else
     prefix="`/bin/cat /var/www/html/dbp.dat`"
 fi
