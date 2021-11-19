9e00f7219aa6815667a7d125d0f082c00cca532b
Agile Deployer <agile-deployer@users.noreply.github.com>
2021-11-19 12:49:31 +0000

    Update PerformPostProcessing.sh

--- providerscripts/application/processing/joomla/PerformPostProcessing.sh
+++ providerscripts/application/processing/joomla/PerformPostProcessing.sh
@@ -121,7 +121,7 @@ then
             command="${SUDO} /bin/sed -i \"s/ENGINE=[a-zA-Z0-9]* /ENGINE=InnoDB /g\" /tmp/supports.sql" && eval ${command} 
             command="${SUDO} /bin/sed -i \"s/ENGINE=[a-zA-Z0-9]* /ENGINE=InnoDB /g\" /var/www/html/administrator/components/com_finder/sql/install.mysql.sql" && eval ${command} 
             command="${SUDO} /bin/sed -i 's/\$this->toggleTables(false);/\/\/\$this->toggleTables(false);/g' /var/www/html/administrator/components/com_finder/src/Indexer/Indexer.php" && eval ${command} 
-            command="${SUDO} /bin/sed -i 's/\$this->toggleTables(false);/\/\/\$this->toggleTables(true);/g' /var/www/html/administrator/components/com_finder/src/Indexer/Indexer.php" && eval ${command} 
+            command="${SUDO} /bin/sed -i 's/\$this->toggleTables(true);/\/\/\$this->toggleTables(true);/g' /var/www/html/administrator/components/com_finder/src/Indexer/Indexer.php" && eval ${command} 
         fi
     else 
         installationstatus="3"
