38a4b38a7a545d0e6cc1625d6479ce2055e4b7db
Agile Deployer <agile-deployer@users.noreply.github.com>
2021-11-24 17:47:29 +0000

    Update BuildLighttpdFromSource.sh

--- installscripts/lighttpd/BuildLighttpdFromSource.sh
+++ installscripts/lighttpd/BuildLighttpdFromSource.sh
@@ -1,3 +1,25 @@
+#!/bin/sh
+######################################################################################
+# Author : Peter Winter
+# Date   : 11/08/2021
+# Description: This will install the lighttpd webserver from source
+#######################################################################################
+# License Agreement:
+# This file is part of The Agile Deployment Toolkit.
+# The Agile Deployment Toolkit is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# The Agile Deployment Toolkit is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with The Agile Deployment Toolkit.  If not, see <http://www.gnu.org/licenses/>.
+########################################################################################
+########################################################################################
+#set -x
+
 /usr/bin/apt-get install -qq -y autoconf automake libtool m4 pkg-config 
 /usr/bin/apt install -qq -y bzip2 libgeoip-dev gnutls-bin gnutls-dev libmaxminddb-dev libxml2 libmariadb-dev libpq-dev zlib1g-dev libssl-dev libpcre3-dev
 
