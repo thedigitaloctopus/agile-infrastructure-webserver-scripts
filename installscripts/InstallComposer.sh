b73cff966187c98656329a0ab7c285848cb637bf
Agile Deployer <agile-deployer@users.noreply.github.com>
2021-11-25 22:14:11 +0000

    Update InstallComposer.sh

--- installscripts/InstallComposer.sh
+++ installscripts/InstallComposer.sh
@@ -30,7 +30,7 @@ then
     /usr/bin/php -r "if (hash_file('sha384', 'composer-setup.php') === '906a84df04cea2aa72f40b5f787e49f22d4c2f19492ac310e8cba5b96ac8b64115ac402c8cd292b8a03482574915d1a8') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
     /usr/bin/php composer-setup.php
     /usr/bin/php -r "unlink('composer-setup.php');"
-    /bin/mv composer /usr/local/bin
+    /bin/mv composer.phar /usr/local/bin/composer
 fi
 
 if ( [ "${BUILDOS}" = "debian" ] )
@@ -39,5 +39,5 @@ then
     /usr/bin/php -r "if (hash_file('sha384', 'composer-setup.php') === '906a84df04cea2aa72f40b5f787e49f22d4c2f19492ac310e8cba5b96ac8b64115ac402c8cd292b8a03482574915d1a8') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
     /usr/bin/php composer-setup.php
     /usr/bin/php -r "unlink('composer-setup.php');"
-    /bin/mv composer /usr/local/bin
+    /bin/mv composer.phar /usr/local/bin/composer
 fi
