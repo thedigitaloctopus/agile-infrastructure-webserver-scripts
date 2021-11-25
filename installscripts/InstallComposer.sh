d8604b2e2f064c6ca7abc1f803bc8ac5540d1a6c
Agile Deployer <agile-deployer@users.noreply.github.com>
2021-11-25 21:48:38 +0000

    Update InstallComposer.sh

--- installscripts/InstallComposer.sh
+++ installscripts/InstallComposer.sh
@@ -1,5 +1,41 @@
 #!/bin/sh
-/usr/bin/php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-/usr/bin/php -r "if (hash_file('sha384', 'composer-setup.php') === '906a84df04cea2aa72f40b5f787e49f22d4c2f19492ac310e8cba5b96ac8b64115ac402c8cd292b8a03482574915d1a8') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
-/usr/bin/php composer-setup.php
-/usr/bin/php -r "unlink('composer-setup.php');"
+######################################################################################################
+# Description: This script will install composer
+# Author: Peter Winter
+# Date: 17/01/2017
+#######################################################################################################
+# License Agreement:
+# This file is part of The Agile Deployment Toolkit.
+# The Agile Deployment Toolkit is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# The Agile Deployment Toolkit is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with The Agile Deployment Toolkit.  If not, see <http://www.gnu.org/licenses/>.
+#######################################################################################################
+#######################################################################################################
+
+if ( [ "${1}" != "" ] )
+then
+    BUILDOS="${1}"
+fi
+
+if ( [ "${BUILDOS}" = "ubuntu" ] )
+then
+    /usr/bin/php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+    /usr/bin/php -r "if (hash_file('sha384', 'composer-setup.php') === '906a84df04cea2aa72f40b5f787e49f22d4c2f19492ac310e8cba5b96ac8b64115ac402c8cd292b8a03482574915d1a8') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+    /usr/bin/php composer-setup.php
+    /usr/bin/php -r "unlink('composer-setup.php');"
+fi
+
+if ( [ "${BUILDOS}" = "debian" ] )
+then
+    /usr/bin/php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+    /usr/bin/php -r "if (hash_file('sha384', 'composer-setup.php') === '906a84df04cea2aa72f40b5f787e49f22d4c2f19492ac310e8cba5b96ac8b64115ac402c8cd292b8a03482574915d1a8') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+    /usr/bin/php composer-setup.php
+    /usr/bin/php -r "unlink('composer-setup.php');"
+fi
