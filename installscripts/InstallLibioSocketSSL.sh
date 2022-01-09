f9c2a9ca1643bbfd5bc2ec8d072b39d95b7fec6b
Agile Deployer <agile-deployer@users.noreply.github.com>
2022-01-09 18:47:32 +0000

    Update InstallLibioSocketSSL.sh

--- installscripts/InstallLibioSocketSSL.sh
+++ installscripts/InstallLibioSocketSSL.sh
@@ -27,11 +27,11 @@ fi
 
 if ( [ "${BUILDOS}" = "ubuntu" ] )
 then
-    /usr/bin/apt-get -qq install libio-socket-ssl-perl
+    /usr/bin/apt-get -o DPkg::Lock::Timeout=-1 -qq install libio-socket-ssl-perl
 fi
 
 if ( [ "${BUILDOS}" = "debian" ] )
 then
-    /usr/bin/apt-get -qq install libio-socket-ssl-perl
+    /usr/bin/apt-get -o DPkg::Lock::Timeout=-1 -qq install libio-socket-ssl-perl
 fi
 
