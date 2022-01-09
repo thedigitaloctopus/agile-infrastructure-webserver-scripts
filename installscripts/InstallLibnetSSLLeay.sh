fd91b5c5bed48346f7545162c66549aec36dcbe2
Agile Deployer <agile-deployer@users.noreply.github.com>
2022-01-09 18:47:54 +0000

    Update InstallLibnetSSLLeay.sh

--- installscripts/InstallLibnetSSLLeay.sh
+++ installscripts/InstallLibnetSSLLeay.sh
@@ -27,11 +27,11 @@ fi
 
 if ( [ "${BUILDOS}" = "ubuntu" ] )
 then
-    /usr/bin/apt-get -qq install libnet-ssleay-perl
+    /usr/bin/apt-get -o DPkg::Lock::Timeout=-1 -qq install libnet-ssleay-perl
 fi
 
 if ( [ "${BUILDOS}" = "debian" ] )
 then
-    /usr/bin/apt-get -qq install libnet-ssleay-perl
+    /usr/bin/apt-get -o DPkg::Lock::Timeout=-1 -qq install libnet-ssleay-perl
 fi
 
