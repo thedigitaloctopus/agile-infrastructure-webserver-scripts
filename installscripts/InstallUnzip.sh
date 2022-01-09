170e3ef3b0c940788fa0ffd51edf8b4c27f0df7a
Agile Deployer <agile-deployer@users.noreply.github.com>
2022-01-09 18:56:33 +0000

    Update InstallUnzip.sh

--- installscripts/InstallUnzip.sh
+++ installscripts/InstallUnzip.sh
@@ -27,11 +27,11 @@ fi
 
 if ( [ "${BUILDOS}" = "ubuntu" ] )
 then
-    /usr/bin/apt-get -qq install unzip
+    /usr/bin/apt-get  -o DPkg::Lock::Timeout=-1 -qq install unzip
 fi
 
 if ( [ "${BUILDOS}" = "debian" ] )
 then
-    /usr/bin/apt-get -qq install unzip
+    /usr/bin/apt-get  -o DPkg::Lock::Timeout=-1 -qq install unzip
 fi
 
