d0cb3e38166c95ef9cff6a6bb9b64466d4e3bf6a
Agile Deployer <agile-deployer@users.noreply.github.com>
2022-01-09 18:56:14 +0000

    Update InstallUFW.sh

--- installscripts/InstallUFW.sh
+++ installscripts/InstallUFW.sh
@@ -27,11 +27,11 @@ fi
 
 if ( [ "${BUILDOS}" = "ubuntu" ] )
 then
-    /usr/bin/apt-get -qq install ufw
+    /usr/bin/apt-get -o DPkg::Lock::Timeout=-1  -qq install ufw
 fi
 
 if ( [ "${BUILDOS}" = "debian" ] )
 then
-    /usr/bin/apt-get -qq install ufw
+    /usr/bin/apt-get -o DPkg::Lock::Timeout=-1  -qq install ufw
 fi
 
