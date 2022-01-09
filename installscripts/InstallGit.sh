f5d6d0f3845a03f1265cae18501588df57416ef8
Agile Deployer <agile-deployer@users.noreply.github.com>
2022-01-09 18:46:28 +0000

    Update InstallGit.sh

--- installscripts/InstallGit.sh
+++ installscripts/InstallGit.sh
@@ -27,11 +27,11 @@ fi
 
 if ( [ "${BUILDOS}" = "ubuntu" ] )
 then
-    /usr/bin/apt-get install -qq git
+    /usr/bin/apt-get -o DPkg::Lock::Timeout=-1 install -qq git
 fi
 
 if ( [ "${BUILDOS}" = "debian" ] )
 then
-    /usr/bin/apt-get install -qq git
+    /usr/bin/apt-get -o DPkg::Lock::Timeout=-1 install -qq git
 fi
 
