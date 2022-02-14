eb7d971e76619b4af627b6eabed23cf249826481
Agile Deployer <agile-deployer@users.noreply.github.com>
2022-02-14 14:08:57 +0000

    Update GatewayGuardian.sh

--- security/GatewayGuardian.sh
+++ security/GatewayGuardian.sh
@@ -34,70 +34,50 @@ fi
 
 if ( [ "`${HOME}/providerscripts/utilities/CheckConfigValue.sh APPLICATION:joomla`" = "1" ] )
 then
-    if ( [ -f ${HOME}/config/GATEWAY_GUARDIAN_UPDATED ] || [ ! -f /etc/basicauth/.htpasswd ] )
+    if ( [ "`${HOME}/providerscripts/utilities/CheckConfigValue.sh BUILDARCHIVECHOICE:virgin`" = "1" ] && [ ! -f ${HOME}/runtime/VIRGINADJUSTED ] )
     then
-        if ( [ "`${HOME}/providerscripts/utilities/CheckConfigValue.sh BUILDARCHIVECHOICE:virgin`" = "1" ] && [ ! -f ${HOME}/runtime/VIRGINADJUSTED ] )
-        then
-            /usr/bin/s3cmd mv s3://gatewayguardian-${BUILD_IDENTIFIER}/htpasswd s3://gatewayguardian-${BUILD_IDENTIFIER}/htpasswd.$$
-            /bin/touch ${HOME}/runtime/VIRGINADJUSTED
-        fi
-        /usr/bin/s3cmd get --force s3://gatewayguardian-${BUILD_IDENTIFIER}/htpasswd
-        /bin/mv htpasswd /etc/basicauth/.htpasswd
-        /bin/chown www-data.www-data /etc/basicauth/.htpasswd
-        /bin/sleep 130
-        /bin/rm ${HOME}/config/credentials/GATEWAY_GUARDIAN_UPDATED
+        /usr/bin/s3cmd mv s3://gatewayguardian-${BUILD_IDENTIFIER}/htpasswd s3://gatewayguardian-${BUILD_IDENTIFIER}/htpasswd.$$
+        /bin/touch ${HOME}/runtime/VIRGINADJUSTED
     fi
+    /usr/bin/s3cmd get --force s3://gatewayguardian-${BUILD_IDENTIFIER}/htpasswd
+    /bin/mv htpasswd /etc/basicauth/.htpasswd
+    /bin/chown www-data.www-data /etc/basicauth/.htpasswd
 fi
 
 if ( [ "`${HOME}/providerscripts/utilities/CheckConfigValue.sh APPLICATION:wordpress`" = "1" ] )
 then
-    if ( [ -f ${HOME}/config/GATEWAY_GUARDIAN_UPDATED ] || [ ! -f /etc/basicauth/.htpasswd ] )
+    if ( [ "`${HOME}/providerscripts/utilities/CheckConfigValue.sh BUILDARCHIVECHOICE:virgin`" = "1" ] && [ ! -f ${HOME}/runtime/VIRGINADJUSTED ] )
     then
-        if ( [ "`${HOME}/providerscripts/utilities/CheckConfigValue.sh BUILDARCHIVECHOICE:virgin`" = "1" ] && [ ! -f ${HOME}/runtime/VIRGINADJUSTED ] )
-        then
-            /usr/bin/s3cmd mv s3://gatewayguardian-${BUILD_IDENTIFIER}/htpasswd s3://gatewayguardian-${BUILD_IDENTIFIER}/htpasswd.$$
-            /bin/touch ${HOME}/runtime/VIRGINADJUSTED
-        fi
-        /usr/bin/s3cmd get --force s3://gatewayguardian-${BUILD_IDENTIFIER}/htpasswd
-        /bin/mv htpasswd /etc/basicauth/.htpasswd
-        /bin/chown www-data.www-data /etc/basicauth/.htpasswd
-        /bin/sleep 130
-        /bin/rm ${HOME}/config/credentials/GATEWAY_GUARDIAN_UPDATED
+        /usr/bin/s3cmd mv s3://gatewayguardian-${BUILD_IDENTIFIER}/htpasswd s3://gatewayguardian-${BUILD_IDENTIFIER}/htpasswd.$$
+        /bin/touch ${HOME}/runtime/VIRGINADJUSTED
     fi
+    /usr/bin/s3cmd get --force s3://gatewayguardian-${BUILD_IDENTIFIER}/htpasswd
+    /bin/mv htpasswd /etc/basicauth/.htpasswd
+    /bin/chown www-data.www-data /etc/basicauth/.htpasswd
 fi
 
 if ( [ "`${HOME}/providerscripts/utilities/CheckConfigValue.sh APPLICATION:drupal`" = "1" ] )
 then
-    if ( [ -f ${HOME}/config/GATEWAY_GUARDIAN_UPDATED ] || [ ! -f /etc/basicauth/.htpasswd ] )
+    if ( [ "`${HOME}/providerscripts/utilities/CheckConfigValue.sh BUILDARCHIVECHOICE:virgin`" = "1" ] && [ ! -f ${HOME}/runtime/VIRGINADJUSTED ] )
     then
-        if ( [ "`${HOME}/providerscripts/utilities/CheckConfigValue.sh BUILDARCHIVECHOICE:virgin`" = "1" ] && [ ! -f ${HOME}/runtime/VIRGINADJUSTED ] )
-        then
-            /usr/bin/s3cmd mv s3://gatewayguardian-${BUILD_IDENTIFIER}/htpasswd s3://gatewayguardian-${BUILD_IDENTIFIER}/htpasswd.$$
-            /bin/touch ${HOME}/runtime/VIRGINADJUSTED
-        fi
-        
-        /usr/bin/s3cmd get --force s3://gatewayguardian-${BUILD_IDENTIFIER}/htpasswd
-        /bin/mv htpasswd /etc/basicauth/.htpasswd
-        /bin/chown www-data.www-data /etc/basicauth/.htpasswd
-        /bin/sleep 130
-        /bin/rm ${HOME}/config/credentials/GATEWAY_GUARDIAN_UPDATED
+        /usr/bin/s3cmd mv s3://gatewayguardian-${BUILD_IDENTIFIER}/htpasswd s3://gatewayguardian-${BUILD_IDENTIFIER}/htpasswd.$$
+        /bin/touch ${HOME}/runtime/VIRGINADJUSTED
     fi
+        
+    /usr/bin/s3cmd get --force s3://gatewayguardian-${BUILD_IDENTIFIER}/htpasswd
+    /bin/mv htpasswd /etc/basicauth/.htpasswd
+    /bin/chown www-data.www-data /etc/basicauth/.htpasswd
 fi
 
 if ( [ "`${HOME}/providerscripts/utilities/CheckConfigValue.sh APPLICATION:moodle`" = "1" ] )
 then
-    if ( [ -f ${HOME}/config/GATEWAY_GUARDIAN_UPDATED ] || [ ! -f /etc/basicauth/.htpasswd ] )
+    if ( [ "`${HOME}/providerscripts/utilities/CheckConfigValue.sh BUILDARCHIVECHOICE:virgin`" = "1" ] && [ ! -f ${HOME}/runtime/VIRGINADJUSTED ] )
     then
-        if ( [ "`${HOME}/providerscripts/utilities/CheckConfigValue.sh BUILDARCHIVECHOICE:virgin`" = "1" ] && [ ! -f ${HOME}/runtime/VIRGINADJUSTED ] )
-        then
-            /usr/bin/s3cmd mv s3://gatewayguardian-${BUILD_IDENTIFIER}/htpasswd s3://gatewayguardian-${BUILD_IDENTIFIER}/htpasswd.$$
-            /bin/touch ${HOME}/runtime/VIRGINADJUSTED
-        fi
-        
-        /usr/bin/s3cmd get --force s3://gatewayguardian-${BUILD_IDENTIFIER}/htpasswd
-        /bin/mv htpasswd /etc/basicauth/.htpasswd
-        /bin/chown www-data.www-data /etc/basicauth/.htpasswd
-        /bin/sleep 130
-        /bin/rm ${HOME}/config/credentials/GATEWAY_GUARDIAN_UPDATED
-    fi
+        /usr/bin/s3cmd mv s3://gatewayguardian-${BUILD_IDENTIFIER}/htpasswd s3://gatewayguardian-${BUILD_IDENTIFIER}/htpasswd.$$
+        /bin/touch ${HOME}/runtime/VIRGINADJUSTED
+    fi    
+    /usr/bin/s3cmd get --force s3://gatewayguardian-${BUILD_IDENTIFIER}/htpasswd
+    /bin/mv htpasswd /etc/basicauth/.htpasswd
+    /bin/chown www-data.www-data /etc/basicauth/.htpasswd
+
 fi
