5dfc6b2743743310a350bf9772f65a3abf36ec9c
Agile Deployer <agile-deployer@users.noreply.github.com>
2022-02-18 13:59:54 +0000

    Update mysqlalive.php

--- providerscripts/utilities/dbalive/mysqlalive.php
+++ providerscripts/utilities/dbalive/mysqlalive.php
@@ -5,6 +5,17 @@ $password = $argv[3];
 $dbname = $argv[4];
 $serverport = (int) $argv[5];
 
+//Make sure the database exists which it might not if we are deploying from snapshots to a managed database
+
+$conn = new mysqli($servername, $username, $password, "", $serverport);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Create database - this will usually error out but that's OK. 
+$sql = "CREATE DATABASE ${dbname}";
+
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname, $serverport);
 // Check connection
