7bdcdf48c998f7ea43a67a9cfd74e98a9aa0715b
Agile Deployer <agile-deployer@users.noreply.github.com>
2022-02-21 09:27:04 +0000

    Update InstallPostgresClient.sh

--- installscripts/InstallPostgresClient.sh
+++ installscripts/InstallPostgresClient.sh
@@ -31,9 +31,8 @@ then
 
     /usr/bin/wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
     /bin/echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" | sudo tee /etc/apt/sources.list.d/pgdg.list
-   # /usr/bin/apt-get update
     ${HOME}/installscripts/UpdateAndUpgrade.sh ${BUILDOS}
-   /usr/bin/apt-get -o DPkg::Lock::Timeout=-1  -y install postgresql-client-`/bin/echo ${version} | /usr/bin/awk -F'.' '{print $1}'`
+    /usr/bin/apt-get -o DPkg::Lock::Timeout=-1  -y install postgresql-client-`/bin/echo ${version} | /usr/bin/awk -F'.' '{print $1}'`
 fi
 
 if ( [ "${BUILDOS}" = "debian" ] )
@@ -42,7 +41,6 @@ then
 
     /usr/bin/wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
     /bin/echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" | sudo tee /etc/apt/sources.list.d/pgdg.list
-   # /usr/bin/apt-get update
     ${HOME}/installscripts/UpdateAndUpgrade.sh ${BUILDOS}
     /usr/bin/apt-get -o DPkg::Lock::Timeout=-1  -y install postgresql-client-`/bin/echo ${version} | /usr/bin/awk -F'.' '{print $1}'`
 fi
