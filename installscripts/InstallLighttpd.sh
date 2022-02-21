4472251b6fe5d3b198ca66e2f2dc2fb2f9d45168
Agile Deployer <agile-deployer@users.noreply.github.com>
2022-02-21 09:23:51 +0000

    Update InstallLighttpd.sh

--- installscripts/InstallLighttpd.sh
+++ installscripts/InstallLighttpd.sh
@@ -31,9 +31,7 @@ then
     
     if ( [ "`${HOME}/providerscripts/utilities/CheckBuildStyle.sh 'LIGHTTPD:source'`" = "1" ] )
     then
-        #${HOME}/installscripts/Update.sh ${BUILDOS}
         ${HOME}/installscripts/UpdateAndUpgrade.sh ${BUILDOS}
-
         ${HOME}/installscripts/lighttpd/BuildLighttpdFromSource.sh 
         /bin/touch /etc/lighttpd/BUILT_FROM_SOURCE
     elif ( [ "`${HOME}/providerscripts/utilities/CheckBuildStyle.sh 'LIGHTTPD:repo'`" = "1" ] )
@@ -48,9 +46,7 @@ if ( [ "${BUILDOS}" = "debian" ] )
 then
     if ( [ "`${HOME}/providerscripts/utilities/CheckBuildStyle.sh 'LIGHTTPD:source'`" = "1" ] )
     then
-        #${HOME}/installscripts/Update.sh ${BUILDOS}
         ${HOME}/installscripts/UpdateAndUpgrade.sh ${BUILDOS}
-
         ${HOME}/installscripts/lighttpd/BuildLighttpdFromSource.sh 
         /bin/touch /etc/lighttpd/BUILT_FROM_SOURCE
     elif ( [ "`${HOME}/providerscripts/utilities/CheckBuildStyle.sh 'LIGHTTPD:repo'`" = "1" ] )
