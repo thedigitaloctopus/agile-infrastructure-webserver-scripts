ebfe54cac865b5ee335950535ed2f212c8af3320
Agile Deployer <agile-deployer@users.noreply.github.com>
2022-02-22 12:32:56 +0000

    Update MonitorS3FS.sh

--- providerscripts/utilities/MonitorS3FS.sh
+++ providerscripts/utilities/MonitorS3FS.sh
@@ -1,5 +1,17 @@
 #!/bin/sh
 
+
+
+if ( [ "`${HOME}/providerscripts/utilities/CheckConfigValue.sh ENABLEEFS:1`" = "1" ] )
+then
+   exit
+fi
+
+if ( [ "`/usr/bin/awk '{print int(($1%3600)/60)}' /proc/uptime`" -lt "3" ] )
+then
+    exit
+fi
+
 /bin/touch ${HOME}/runtime/PERFORMING_S3FS_CHECK 
 /bin/touch ${HOME}/runtime/MONITOR_S3FS
 
