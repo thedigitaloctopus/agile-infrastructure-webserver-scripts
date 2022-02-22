99f14140ac02c6c3a3c724c6159c73c00c192b09
Agile Deployer <agile-deployer@users.noreply.github.com>
2022-02-22 12:18:55 +0000

    Update CheckS3Fs.sh

--- providerscripts/utilities/CheckS3Fs.sh
+++ providerscripts/utilities/CheckS3Fs.sh
@@ -2,13 +2,23 @@
 
 count=0
 
-while ( [ "${count}" -lt "10" ] )
+/bin/sleep 10
+
+while ( [ "${count}" -lt "5" ] )
 do
     if ( [ -f ${HOME}/runtime/PERFORMING_S3FS_CHECK ] && [ ! -f ${HOME}/runtime/MONITOR_S3FS ] )
     then
         /bin/rm ${HOME}/runtime/PERFORMING_S3FS_CHECK ${HOME}/runtime/MONITOR_S3FS
+        exit
     else
-        shutdown -r now
+        if ( [ -f ${HOME}/runtime/PERFORMING_S3FS_CHECK ] )
+        then
+            /bin/rm ${HOME}/runtime/PERFORMING_S3FS_CHECK
+            /bin/echo "${0} `/bin/date`: Emergency reboot has happened because s3fs looks to have become unresponsive which hoses us if we don't reboot" >> ${HOME}/logs/UnresponsiveS3FSLog.dat
+            /usr/sbin/shutdown -r now
+        fi
     fi
+    /bin/sleep 10
+    count="`/usr/bin/expr ${count} + 1`"
 done
         
