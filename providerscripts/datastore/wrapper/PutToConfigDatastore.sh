c139521554057ef64706571988be098e2dab7150
Agile Deployer <agile-deployer@users.noreply.github.com>
2022-02-24 00:00:41 +0000

    Create PutToConfigDatastore.sh

---
+++ providerscripts/datastore/wrapper/PutToConfigDatastore.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+#set -x
+
+WEBSITE_URL="ok.nuocial.org.uk"
+configbucket="`/bin/echo ${WEBSITE_URL} | /usr/bin/awk -F'.' '{ for(i = 1; i <= NF; i++) { print $i; } }' | /usr/bin/cut -c1-3 | /usr/bin/tr '\n' '-' | /bin/sed 's/-//g'`"
+configbucket="${configbucket}-config"
+
+/usr/bin/s3cmd put $1 s3://${configbucket}/$2
