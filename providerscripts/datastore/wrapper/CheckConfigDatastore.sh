a744c4f116b47530d305d6899f676d7eefe611b0
Agile Deployer <agile-deployer@users.noreply.github.com>
2022-02-24 00:08:48 +0000

    Update CheckConfigDatastore.sh

--- providerscripts/datastore/wrapper/CheckConfigDatastore.sh
+++ providerscripts/datastore/wrapper/CheckConfigDatastore.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 #set -x
-
+export HOME=`/bin/cat /home/homedir.dat`
 WEBSITE_URL="`${HOME}/providerscripts/utilities/ExtractConfigValue.sh 'WEBSITEURL'`"
 configbucket="`/bin/echo ${WEBSITE_URL} | /usr/bin/awk -F'.' '{ for(i = 1; i <= NF; i++) { print $i; } }' | /usr/bin/cut -c1-3 | /usr/bin/tr '\n' '-' | /bin/sed 's/-//g'`"
 configbucket="${configbucket}-config"
