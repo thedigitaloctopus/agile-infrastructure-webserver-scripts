80cf76649b1aada6b773454237557e097fa8922b
Agile Deployer <agile-deployer@users.noreply.github.com>
2022-02-24 19:00:45 +0000

    Create DeleteFromConfigDatastore.sh

---
+++ providerscripts/datastore/wrapper/DeleteFromConfigDatastore.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+export HOME=`/bin/cat /home/homedir.dat`
+WEBSITE_URL="`${HOME}/providerscripts/utilities/ExtractConfigValue.sh 'WEBSITEURL'`"
+configbucket="`/bin/echo ${WEBSITE_URL} | /usr/bin/awk -F'.' '{ for(i = 1; i <= NF; i++) { print $i; } }' | /usr/bin/cut -c1-3 | /usr/bin/tr '\n' '-' | /bin/sed 's/-//g'`"
+configbucket="${configbucket}-config"
+
+/usr/bin/s3cmd --force del s3://${configbucket}/$1
