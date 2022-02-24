0d91f2710bb832faba81ef7b04d5e14d091935a4
Agile Deployer <agile-deployer@users.noreply.github.com>
2022-02-24 18:50:18 +0000

    Update CheckConfigDatastore.sh

--- providerscripts/datastore/wrapper/CheckConfigDatastore.sh
+++ providerscripts/datastore/wrapper/CheckConfigDatastore.sh
@@ -1,6 +1,25 @@
 #!/bin/sh
-
+####################################################################################
+# Author: Peter Winter
+# Date :  9/4/2016
+# Description: This script will check for a particular value in the configuration datastore
+#######################################################################################
+# License Agreement:
+# This file is part of The Agile Deployment Toolkit.
+# The Agile Deployment Toolkit is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# The Agile Deployment Toolkit is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with The Agile Deployment Toolkit.  If not, see <http://www.gnu.org/licenses/>.
+######################################################################################
+######################################################################################
 #set -x
+
 export HOME=`/bin/cat /home/homedir.dat`
 WEBSITE_URL="`${HOME}/providerscripts/utilities/ExtractConfigValue.sh 'WEBSITEURL'`"
 configbucket="`/bin/echo ${WEBSITE_URL} | /usr/bin/awk -F'.' '{ for(i = 1; i <= NF; i++) { print $i; } }' | /usr/bin/cut -c1-3 | /usr/bin/tr '\n' '-' | /bin/sed 's/-//g'`"
