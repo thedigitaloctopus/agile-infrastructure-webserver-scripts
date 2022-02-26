bcbd71389d8bc96bfd7f2d681e9554b2d8e6a3eb
Agile Deployer <agile-deployer@users.noreply.github.com>
2022-02-26 10:47:29 +0000

    Update CheckServerAlive.sh

--- providerscripts/application/monitoring/CheckServerAlive.sh
+++ providerscripts/application/monitoring/CheckServerAlive.sh
@@ -28,9 +28,9 @@ then
     exit
 fi
 
-DB_N="`/bin/sed '1q;d' ${HOME}/config/credentials/shit`"
-DB_P="`/bin/sed '2q;d' ${HOME}/config/credentials/shit`"
-DB_U="`/bin/sed '3q;d' ${HOME}/config/credentials/shit`"
+DB_N="`${HOME}/providerscripts/datastore/configwrapper/GetDBCredential.sh "credentials/shit" 1`"
+DB_P="`${HOME}/providerscripts/datastore/configwrapper/GetDBCredential.sh "credentials/shit" 2`"
+DB_U="`${HOME}/providerscripts/datastore/configwrapper/GetDBCredential.sh "credentials/shit" 3`"
 
 if ( [ ! -f ${HOME}/runtime/APPLICATION_CONFIGURATION_PREPARED ] )
 then
