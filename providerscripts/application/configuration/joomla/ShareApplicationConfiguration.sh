adb2d0e60f400f50aa6b1a0fed1bb9497c6e37e8
Agile Deployer <agile-deployer@users.noreply.github.com>
2022-02-28 12:59:00 +0000

    Update ShareApplicationConfiguration.sh

--- providerscripts/application/configuration/joomla/ShareApplicationConfiguration.sh
+++ providerscripts/application/configuration/joomla/ShareApplicationConfiguration.sh
@@ -85,7 +85,7 @@ changed=""
 
 if ( [ "${updated}" = "1" ] )
 then
-    if ( [ "`/usr/bin/find ${HOME}/config/joomla_configuration.php -mmin -2`" != "" ] )
+    if ( [ "`/usr/bin/find ${HOME}/runtime/APP_CONFIG_UPDATED -mmin -2`" != "" ] )
     then
         #This check is needed so we don't accidentally update the config file and push it to all our webservers
         #We have to explicitly create the GLOBAL_CONFIG_UPDATE file to do that. 
