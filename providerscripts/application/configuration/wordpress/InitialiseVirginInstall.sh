dd5e612e3216634d4c8bcf196d1cfb9aad12de7f
Agile Deployer <agile-deployer@users.noreply.github.com>
2022-02-28 18:15:52 +0000

    Update InitialiseVirginInstall.sh

--- providerscripts/application/configuration/wordpress/InitialiseVirginInstall.sh
+++ providerscripts/application/configuration/wordpress/InitialiseVirginInstall.sh
@@ -100,7 +100,7 @@ fi
 #    exit
 #fi
 
-if ( [ "`${HOME}/providerscripts/datastore/configwrapper/CheckConfigDatastore.sh "credentials/shit"`" = "1" ] )
+if ( [ "`${HOME}/providerscripts/datastore/configwrapper/CheckConfigDatastore.sh "credentials/shit"`" = "0" ] )
 then
    exit
 fi
