f05bfb3a7decab7b9ba159734963a767aa887663
Agile Deployer <agile-deployer@users.noreply.github.com>
2022-03-01 22:49:30 +0000

    Update InstallPythonPIP.sh

--- installscripts/InstallPythonPIP.sh
+++ installscripts/InstallPythonPIP.sh
@@ -27,6 +27,7 @@ fi
 
 if ( [ "${BUILDOS}" = "ubuntu" ] )
 then
+    /usr/bin/apt-get -o DPkg::Lock::Timeout=-1 -qq -y install libmagic-dev
     /usr/bin/apt-get -o DPkg::Lock::Timeout=-1  -qq -y -f install python3-pip
     /bin/rm /usr/bin/python
     /bin/ln -s /usr/bin/python3 /usr/bin/python
@@ -37,6 +38,7 @@ fi
 
 if ( [ "${BUILDOS}" = "debian" ] )
 then
+    /usr/bin/apt-get -o DPkg::Lock::Timeout=-1 -qq -y install libmagic-dev
     /usr/bin/apt-get -o DPkg::Lock::Timeout=-1  -qq -y -f install python3-pip
     /bin/rm /usr/bin/python
     /bin/ln -s /usr/bin/python3 /usr/bin/python
