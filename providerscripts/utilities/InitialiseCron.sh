0da29844846edc9d79e476f53e9dd683b4c2585a
Agile Deployer <agile-deployer@users.noreply.github.com>
2022-03-08 19:12:21 +0000

    Update InitialiseCron.sh

--- providerscripts/utilities/InitialiseCron.sh
+++ providerscripts/utilities/InitialiseCron.sh
@@ -62,7 +62,7 @@ fi
 /bin/echo "*/5 * * * * export HOME="${HOMEDIR}" &&  /bin/sleep 20 && ${HOME}/cron/SyncToWebrootTunnelFromCron.sh && /bin/sleep 60 && ${HOME}/cron/SyncFromWebrootTunnelFromCron.sh" >> /var/spool/cron/crontabs/root
 
 #This script runs every 30 minutes
-/bin/echo "*/30 * * * * export HOME="${HOMEDIR}" && ${HOME}/providerscripts/datastore/configwrapper/DeleteFromConfigDatastore.sh SSLUPDATED" >> /var/spool/cron/crontabs/root
+#/bin/echo "*/30 * * * * export HOME="${HOMEDIR}" && ${HOME}/providerscripts/datastore/configwrapper/DeleteFromConfigDatastore.sh SSLUPDATED" >> /var/spool/cron/crontabs/root
 
 #These scripts run at set times these will make a backup of our webroot to git and also to our datastore if super safe
 #Time based backups are not taken for virgin CMS installs. Instead, make a baseline if you want to save a copy of your work and work it out from there once your application is ready
