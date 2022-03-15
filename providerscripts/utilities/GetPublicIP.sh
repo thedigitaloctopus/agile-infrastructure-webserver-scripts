b58b7b56f94eb2c606f9dcedf3fdba75a42a9814
Agile Deployer <agile-deployer@users.noreply.github.com>
2022-03-15 22:55:38 +0000

    Update GetPublicIP.sh

--- providerscripts/utilities/GetPublicIP.sh
+++ providerscripts/utilities/GetPublicIP.sh
@@ -21,12 +21,7 @@
 #set -x
 
 IP="`${HOME}/providerscripts/utilities/ExtractConfigValue.sh 'MYPUBLICIP'`"
-#if ( [ "`/usr/bin/ip addr | /bin/grep ${IP}`" != "" ] )
-#then
-    /bin/echo ${IP}
-#else
-#    exit
-#fi
+/bin/echo ${IP}
 
 
 
