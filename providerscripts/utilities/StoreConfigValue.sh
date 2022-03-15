35a87066590e744ce43fa0721e0c2d6e3102eee9
Agile Deployer <agile-deployer@users.noreply.github.com>
2022-03-15 23:01:38 +0000

    Update StoreConfigValue.sh

--- providerscripts/utilities/StoreConfigValue.sh
+++ providerscripts/utilities/StoreConfigValue.sh
@@ -27,11 +27,9 @@ then
     /bin/sed -i "/.*${1}:/d" ${HOME}/.ssh/webserver_configuration_settings.dat
     /bin/sed -i "\$ a\ ${1}:${2}" ${HOME}/.ssh/webserver_configuration_settings.dat 
     /bin/sed -i "s/^ //g" ${HOME}/.ssh/webserver_configuration_settings.dat 
-   # /bin/echo "${1}:${2}" >> ${HOME}/.ssh/webserver_configuration_settings.dat
 elif ( [ "${1}" != "" ] && [ "${2}" = "" ] )
 then
     /bin/sed -i "/.*${1}$/d" ${HOME}/.ssh/webserver_configuration_settings.dat
     /bin/sed -i "\$ a\ ${1}" ${HOME}/.ssh/webserver_configuration_settings.dat 
     /bin/sed -i "s/^ //g" ${HOME}/.ssh/webserver_configuration_settings.dat 
-  #  /bin/echo "${1}" >> ${HOME}/.ssh/webserver_configuration_settings.dat
 fi
