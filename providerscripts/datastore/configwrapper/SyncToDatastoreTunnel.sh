7ce02c28476420b6458e885627dba0a2a5321dc2
Agile Deployer <agile-deployer@users.noreply.github.com>
2022-03-24 21:41:26 +0000

    Update SyncToDatastoreTunnel.sh

--- providerscripts/datastore/configwrapper/SyncToDatastoreTunnel.sh
+++ providerscripts/datastore/configwrapper/SyncToDatastoreTunnel.sh
@@ -18,7 +18,7 @@
 # along with The Agile Deployment Toolkit.  If not, see <http://www.gnu.org/licenses/>.
 ########################################################################################
 ########################################################################################
-set -x
+#set -x
 
 if ( [ "`${HOME}/providerscripts/datastore/configwrapper/CheckConfigDatastore.sh "credentials/shit"`" = "0" ] )
 then
