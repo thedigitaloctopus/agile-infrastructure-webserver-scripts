9efab87f93af11465feb7bf20dea937774106e91
Agile Deployer <agile-deployer@users.noreply.github.com>
2022-03-30 22:22:13 +0100

    Update SetupAssetsStore.sh

--- providerscripts/datastore/SetupAssetsStore.sh
+++ providerscripts/datastore/SetupAssetsStore.sh
@@ -28,14 +28,9 @@ then
     exit
 fi
 
-if ( [ "`${HOME}/providerscripts/utilities/CheckConfigValue.sh PERSISTASSETSTOCLOUD:1`" != "1" ] )
+if ( [ "`${HOME}/providerscripts/utilities/CheckConfigValue.sh PERSISTASSETSTOCLOUD:1`" != "1" ] && ( [ "`${HOME}/providerscripts/utilities/CheckConfigValue.sh PERSISTASSETSTOCLOUD:0`" != "1" ] || [ "`${HOME}/providerscripts/utilities/CheckConfigValue.sh PERSISTASSETSTOCLOUD:2`" != "1" ] ) )
 then
     exit
-fi
-
-if ( [ "`${HOME}/providerscripts/utilities/CheckConfigValue.sh PERSISTASSETSTOCLOUD:0`" != "1" ] && [ "`${HOME}/providerscripts/utilities/CheckConfigValue.sh PERSISTASSETSTOCLOUD:2`" != "1" ] )
-then
-   exit
 fi
 
 if ( [ "`${HOME}/providerscripts/datastore/configwrapper/CheckConfigDatastore.sh "INSTALLEDSUCCESSFULLY"`" = "0" ] )
