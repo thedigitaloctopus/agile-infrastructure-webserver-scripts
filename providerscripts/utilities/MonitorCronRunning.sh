dc2c83d6bf91cf80851fa1a9119d51e0f0564198
Agile Deployer <agile-deployer@users.noreply.github.com>
2022-04-01 16:43:16 +0100

    Create MonitorCronRunning.sh

---
+++ providerscripts/utilities/MonitorCronRunning.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -x
+
+
+while ( [ 1 ] )
+do
+   if ( [ "`/bin/cat /var/log/syslog | /bin/grep -i CRON | /bin/grep fork | /bin/grep error`" != "" ] )
+   then
+      /bin/sed -i '/.*error.*fork.*/d' /var/log/syslog  
+   fi
+   /bin/sleep 60
+done
