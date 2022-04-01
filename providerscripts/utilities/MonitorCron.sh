78c2d2c6f31e3d5c1de5159ab58f81af6b434494
Agile Deployer <agile-deployer@users.noreply.github.com>
2022-04-01 16:43:57 +0100

    Update MonitorCron.sh

--- providerscripts/utilities/MonitorCron.sh
+++ providerscripts/utilities/MonitorCron.sh
@@ -3,8 +3,9 @@
 
 while ( [ 1 ] )
 do
-   if ( [ "`/bin/cat /var/log/syslog | /bin/grep CRON | /bin/grep fork | /bin/grep error`" != "" ] )
+   if ( [ "`/bin/cat /var/log/syslog | /bin/grep -i CRON | /bin/grep fork | /bin/grep error`" != "" ] )
    then
-      /bin/sed -i '/*error*fork*/d' /var/log/syslog  
+      /bin/sed -i '/.*error.*fork.*/d' /var/log/syslog  
    fi
+   /bin/sleep 60
 done
