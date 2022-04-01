ffc42e9fb964759e97e58c921e86f59a8cac5a81
Agile Deployer <agile-deployer@users.noreply.github.com>
2022-04-01 16:45:29 +0100

    Update MonitorCron.sh

--- providerscripts/utilities/MonitorCron.sh
+++ providerscripts/utilities/MonitorCron.sh
@@ -1,4 +1,25 @@
 #!/bin/sh
+######################################################################################
+# Author : Peter Winter
+# Date   : 11/08/2021
+# Description: If our machine doesn't have much memory, then, cron can have difficulty running
+# so we monitor for it and send an email to the system administrator
+#######################################################################################
+# License Agreement:
+# This file is part of The Agile Deployment Toolkit.
+# The Agile Deployment Toolkit is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# The Agile Deployment Toolkit is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with The Agile Deployment Toolkit.  If not, see <http://www.gnu.org/licenses/>.
+########################################################################################
+########################################################################################
+#set -x
 
 
 while ( [ 1 ] )
